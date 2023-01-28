52621f3849f59a62383aa47378e75df494499b9f
MisileLaboratory <misileminecord@gmail.com>
2023-01-29 04:12:36 +0900

    testing it

--- projects/hardflat/src/main/kotlin/xyz/misilelaboratory/hardflat/main.kt
+++ projects/hardflat/src/main/kotlin/xyz/misilelaboratory/hardflat/main.kt
@@ -1,5 +1,25 @@
 package xyz.misilelaboratory.hardflat
 
-class HardFlat: JavaPlugin() {
+import org.bukkit.entity.LivingEntity
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.plugin.java.JavaPlugin
 
+@Suppress("unused")
+class HardFlat: JavaPlugin() {
+    override fun onEnable() {
+        this.logger.info("Enabled plugin")
+        this.server.pluginManager.registerEvents(EHandler(), this)
+    }
+}
+
+class EHandler: Listener {
+    @EventHandler
+    fun onDamage(e: EntityDamageEvent) {
+        if (e is LivingEntity) {
+            val ent = e as LivingEntity
+            ent.noDamageTicks = 0
+        }
+    }
 }
