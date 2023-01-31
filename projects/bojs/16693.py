5abc48ae65662043809b7301e36c9c217de433dd
misilelaboratory <misileminecord@gmail.com>
2023-02-01 01:03:59 +0900

    new prob
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

---
+++ projects/bojs/16693.py
@@ -0,0 +1,9 @@
+from math import pi
+
+a, b = map(int, input().split(" "))
+c, d = map(int, input().split(" "))
+
+if a / b > c ** 2 * pi / d:
+    print("Slice of pizza")
+else:
+    print("Whole pizza")
---
+++ projects/bojs/17009.py
@@ -0,0 +1,8 @@
+a = sum(int(input()) * i for i in reversed(range(1, 4)))
+b = sum(int(input()) * i for i in reversed(range(1, 4)))
+if a > b:
+    print("A")
+elif b > a:
+    print("B")
+else:
+    print("T")
