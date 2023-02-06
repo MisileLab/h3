4317917594aca1df0dd12b7cdcb910add1a7b139
MisileLaboratory <misileminecord@gmail.com>
2023-02-06 15:45:19 +0900

    yeah new update

--- projects/hardflat/src/main/kotlin/xyz/misilelaboratory/hardflat/main.kt
+++ projects/hardflat/src/main/kotlin/xyz/misilelaboratory/hardflat/main.kt
@@ -2,6 +2,8 @@ package xyz.misilelaboratory.hardflat
 
 import io.papermc.paper.event.player.PrePlayerAttackEntityEvent
 import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.attribute.Attribute
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
@@ -16,12 +18,29 @@ import org.bukkit.event.inventory.CraftItemEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 @Suppress("unused")
 class HardFlat: JavaPlugin() {
     override fun onEnable() {
-        this.logger.info("Enabled plugin")
-        this.server.pluginManager.registerEvents(EHandler(), this)
+        logger.info("Enabled plugin")
+        server.pluginManager.registerEvents(EHandler(), this)
+        var a: World? = null
+        for (i in server.worlds) {
+            if (i.environment == World.Environment.THE_END) {
+                a = i
+                break
+            }
+        }
+        if (a!!.enderDragonBattle != null) {
+            val b = a.enderDragonBattle!!.enderDragon
+            if (b != null) {
+                val c = b.getAttribute(Attribute.GENERIC_MAX_HEALTH)!!
+                c.baseValue = (2000).toDouble()
+                b.registerAttribute(c.attribute)
+            }
+        }
     }
 }
 
@@ -65,6 +84,8 @@ class EHandler: Listener {
         } else if (rand == 1) {
             e.isDropItems = false
             e.player.sendMessage("no item")
+        } else if ((1..10).random() == 1 && (e.block.type == Material.GLOW_BERRIES || e.block.type == Material.SWEET_BERRIES)) {
+            e.player.addPotionEffect(PotionEffect(PotionEffectType.WITHER, 2, 1))
         }
     }
 
@@ -112,6 +133,20 @@ class EHandler: Listener {
             e.isCancelled = true
         } else if (e.item.type in rawFood) {
             e.player.damage((6).toDouble())
+        }
+    }
+
+    @EventHandler
+    fun onFall(e: EntityDamageEvent) {
+        var a = e.entity
+        if (a is Player && e.cause == EntityDamageEvent.DamageCause.FALL) {
+            a = e.entity as Player
+            if ((1..20).random() == 1) {
+                a.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 3, 1))
+            } else if ((1..50).random() == 1) {
+                a.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 3, 1))
+                a.addPotionEffect(PotionEffect(PotionEffectType.WITHER, 3, 1))
+            }
         }
     }
 }
