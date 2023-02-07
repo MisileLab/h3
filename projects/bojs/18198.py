8c06f5e43d758cdddefd27cdde2549ad73c6224c
MisileLaboratory <misileminecord@gmail.com>
2023-02-07 11:27:44 +0000

    new prob

---
+++ projects/bojs/18198.py
@@ -0,0 +1,15 @@
+a, b = (0, 0)
+c = input()
+_prev = ""
+
+for i in c:
+    if _prev == "A":
+        a += int(i)
+    elif _prev == "B":
+        b += int(i)
+    _prev = i
+
+if a > b:
+    print("A")
+else:
+    print("B")
