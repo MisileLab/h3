40f6b7b3dbd7cea15bc9d92d12fff00fff120296
MisileLaboratory <misileminecord@gmail.com>
2023-02-14 03:52:00 +0000

    new prob

--- projects/bojs/9782.py
+++ projects/bojs/9782.py
@@ -1,16 +1,14 @@
-from sys import stdin
-
-def get_median(data):
-    data = sorted(data)
-    centerIndex = len(data)//2
-    return (data[centerIndex] + data[-centerIndex - 1])/2
-
+from statistics import median
 a = []
 
-for i in stdin:
-    if i.strip("\n") in ["0", ""]:
+while True:
+    try:
+        b = input()
+    except EOFError:
         break
-    a.append(list(map(int, i.strip("\n").split(" ")[1:])))
+    if b == "0":
+        break
+    a.append(list(map(int, b.split(" ")[1:])))
 
 for i, i2 in zip(a, range(1, len(a)+1)):
-    print(f"Case {i2}: {get_median(i):.1f}")
+    print(f"Case {i2}: {median(i):.1f}")
