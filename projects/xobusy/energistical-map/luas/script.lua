5ee6de129242a7496f8923169de4c25ac50acb83
MisileLaboratory <misileminecord@gmail.com>
2023-03-20 22:31:11 +0000

    update+

--- projects/xobusy/energistical-map/luas/script.lua
+++ projects/xobusy/energistical-map/luas/script.lua
@@ -1,6 +1,12 @@
+local _last = 0
+local a = peripheral.wrap("back")
 
 while true do
-    a = perhiperal.wrap("back")
-    print(a.getLastInput())
-    term.clear()
+    if _last ~= a.getLastInput() then
+        term.clear()
+        term.setCursorPos(1,1)
+        io.write(a.getLastInput())
+        _last = a.getLastInput()
+    end
+    sleep(0.5)
 end
