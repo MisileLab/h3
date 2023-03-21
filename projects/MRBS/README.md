c8572d294ca9bfa4a1ebbb7cb4331ae94f950550
MisileLaboratory <misileminecord@gmail.com>
2023-03-21 15:36:10 +0000

    backend ready

--- projects/MRBS/README.md
+++ projects/MRBS/README.md
@@ -4,3 +4,19 @@
 
 - Python 3.11
 - Python Poetry
+
+## Developing
+
+We have frontend for CC: Tweaked but not now, I'll upload it
+
+- [ ] Turn on/Turn off
+- [ ] Info
+- [ ] Change Burning Rate
+
+## Planned
+
+- [ ] Auto Start/Stop
+- [ ] Alarm
+- [ ] Fancy graph
+- [ ] Incorrect value detection
+- [ ] Type Sorting for efficiency, performance boost
--- projects/MRBS/main.py
+++ projects/MRBS/main.py
@@ -1,23 +1,37 @@
 from simple_websocket_server import WebSocketServer, WebSocket
+from json import loads, dumps
 
 clients = []
+answers = []
+
+def broadcast(self, msg: str):
+    for client in clients:
+        if self != client:
+            client.send_message(msg)
+
 class AEBSWebsocket(WebSocket):
     def handle(self):
-        self.send_message(f'{self.data} from server')
-        broadcast("asd")
-        print(self.data)
-    
+        if self.data in ["T", "TF", "A", "AS", "I"]:
+            broadcast(self, dumps({"name": self.data}))
+        elif self.data == "ID":
+            try:
+                data = loads(self.data.split(" ")[1])
+                data["name"] = "ID"
+            except Exception:
+                self.send_message(dumps({"name": "IC"}))
+            else:
+                broadcast(self, dumps(data))
+        elif self.data[0] == "B" and self.data.removeprefix("B").isnumeric():
+            broadcast(self, dumps({"name": "B", "rate": int(self.data.removeprefix("B"))}))
+        print(f"{self.address}: {self.data}")
+
     def connected(self):
         print(self.address, 'connected')
         clients.append(self)
 
     def handle_close(self):
         print(self.address, 'closed')
+        clients.remove(self)
 
 server = WebSocketServer('0.0.0.0', 25567, AEBSWebsocket)
-
-def broadcast(msg: str):
-    for client in clients:
-        client.send_message(msg)
-
 server.serve_forever()
