f87bac11f954fb15a7d9ce26291b9775df621492
MisileLaboratory <misileminecord@gmail.com>
2023-03-28 07:20:47 +0000

    bruh boj moment

---
+++ projects/bojs/26500.py
@@ -0,0 +1,3 @@
+for i in [map(float, input().split(" ")) for _ in range(int(input()))]:
+    a, b = i
+    print(f"{max(a, b) - min(a, b):.1f}")
---
+++ projects/bojs/27889.py
@@ -0,0 +1,6 @@
+a = {"NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy"}
+
+print(a[input()])
