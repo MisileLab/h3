ae6509934a1470eefb13842f97fcf34925d9fbb5
MisileLaboratory <misileminecord@gmail.com>
2023-03-31 15:00:45 +0000

    some change

--- projects/bojs/26566.py
+++ projects/bojs/26566.py
@@ -2,5 +2,4 @@ for i in [
     [map(int, input().split(" ")), map(int, input().split(" "))]
     for _ in range(int(input()))
 ]:
-    a, b = i[0], i[1]
-    w, s = a
+    w1, w2, s1, s2 = i[0], i[2]
