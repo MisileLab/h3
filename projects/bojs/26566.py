b7ac44869e78ad6861b71b496b655d5cf3d016c9
MisileLaboratory <misileminecord@gmail.com>
2023-04-02 09:42:28 +0000

    unfinish

--- projects/bojs/26566.py
+++ projects/bojs/26566.py
@@ -1,5 +1,13 @@
+from math import pi
+
 for i in [
     [map(int, input().split(" ")), map(int, input().split(" "))]
     for _ in range(int(input()))
 ]:
-    w1, w2, s1, s2 = i[0], i[2]
+    w1, w2 = i[0]
+    s1, s2 = i[1]
+    r = ((s1 / (2 * pi)) ** 2 * pi) / s2
+    r2 = w1 / w2
+    print((s1 / (2 * pi)) ** 2 * pi)
+    print(r2)
+    print(r)
