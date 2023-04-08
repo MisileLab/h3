9dc0ddc53464b505f99ddfc7af548cf1ba9b4cdb
MisileLaboratory <misileminecord@gmail.com>
2023-04-08 15:22:32 +0000

    some modpacks remove

--- modpacks/mekanically/README.md
+++
@@ -1,4 +0,0 @@
-# Mekanically
-
-Forge 1.18.2\
-Many mekanism
--- modpacks/mekanically/server_scripts/main.js
+++
@@ -1,3 +0,0 @@
-onEvent('item.tags', event => {
-    event.get('forge:ingots/unobtainium').add('createchromaticreturn:multiplite_ingot')
-})
