7687c258aa802bd1d2ea50bddc4f7be0ca4f0acd
MisileLaboratory <misileminecord@gmail.com>
2023-04-25 16:06:02 +0900

    css

---
+++ projects/xobusy2/cs/13/2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[100000];
+    int b, c;
+    printf("문자열 입력 : ");
+    scanf("%s", a);
+    for (int i=0; a[i] != '\0' && i < 100000; i++) {
+        if (a[i] == '(') {
+            b++;
+        } else if (a[i] == ')') {
+            c++;
+        }
+    }
+    printf("여는 괄호 : %d, 닫는 괄호 : %d", b, c);
+}
