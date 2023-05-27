85fba7ce5f636ed662b106ed2165623739c2b04f
misilelab <misileminecord@gmail.com>
2023-05-27 13:23:54 +0900

    unresolve

--- projects/eccts/induction-redstone-recv.lua
+++ projects/eccts/induction-redstone-recv.lua
@@ -1,10 +1,13 @@
 local wsurl = "ws://localhost:1000"
 local ws = http.websocket(wsurl)
 local modem = peripheral.find("modem")
+local map_recv = {
+    pneu = "computer_0"
+}
 
 while true do
     local msg = textutils.unserialiseJSON(ws.receive())
-    if msg['type'] == 'induction' then
-        modem.callRemote("computer_0", "setRedstone", msg['data']['pneu'])
+    if msg['type'] == 'induction_redstone' then
+        modem.callRemote(map_recv[msg['redstone_type']], "setRedstone", msg['redstone_value'])
     end
 end
