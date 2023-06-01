7253c912e279740f9539862c8e6e641b4bbcdae0
misilelab <misileminecord@gmail.com>
2023-06-01 09:57:15 +0900

    time limit bruh

--- projects/ap/bojs/25342.py
+++ projects/ap/bojs/25342.py
@@ -1,19 +1,17 @@
-from functools import reduce
-# https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-135.php
-def test(nums):
-    return reduce(lambda x,y:lcm(x,y),nums)
-def gcd(a, b):
-    while b:
-        a, b = b, a%b
-    return a
-def lcm(a, b):
-    return a*b // gcd(a, b)
+import math
 
-for i in [int(input()) for _ in range(int(input()))]:
-    numbers = list(range(1, i+1))
-    answern = 0
-    for i2 in range(len(numbers) - 2):
-        _temp = test([numbers[i2], numbers[i2+1], numbers[i2+2]])
-        if max(_temp, answern) == _temp:
-            answern = _temp
-    print(answern)
+def lcm(a: int, b: int) -> int:
+    return abs(a * b) // math.gcd(a, b)
+
+def find_largest_lcm(n: int) -> int:
+    answer = 0
+    for i in range(1, n - 1):
+        temp = lcm(i, lcm(i + 1, i + 2))
+        answer = max(answer, temp)
+    return answer
+
+num_cases = int(input(""))
+for _ in range(num_cases):
+    n = int(input(""))
+    largest_lcm = find_largest_lcm(n)
+    print(largest_lcm)
