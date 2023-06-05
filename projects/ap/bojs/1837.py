eee0423c8ee2a2e38e6ffb387036b73e508279af
misilelab <misileminecord@gmail.com>
2023-06-06 01:39:16 +0900

    restruct

--- projects/ap/bojs/1837.py
+++ projects/ap/bojs/1837.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from os import _exit
 
 a, b = map(int, input().split(" "))
 
-for i in range(2, a):
+for i in range(2, a+1):
     if a % i == 0 and i < b:
         print(f"BAD {i}")
         _exit(0)
