a8905e9b6bec192e33a704f36f2ba1b283356fab
misilelab <misileminecord@gmail.com>
2023-06-11 01:25:07 +0900

    working but not eff

--- projects/ap/bojs/20305.py
+++ projects/ap/bojs/20305.py
@@ -1,26 +1,19 @@
-from functools import lru_cache
-
-a = list(range(int(input())))
-b = [map(int, input().split(" ")) for _ in range(int(input()))]
+a = [0] * int(input())
+b = [list(map(int, input().split())) for _ in range(int(input()))]
 e = {}
 
-@lru_cache(maxsize=None)
-def fib(n):
-    if n == 0:
-        return 0
-    elif n in [1, 2]:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+FLOATING_LAND = 10 ** 9 + 7
 
-for i in b:
-    c, d = i
-    for i2, _ in enumerate(range(c, d)):
-        if e.get(i2+1) is None:
-            f = fib(i2)
-            e[i2] = f
-        else:
-            f = e[i2+1]
-        a[i2] += f
+def fibonacci(n):
+    fib = [0, 1]
+    fib.extend((fib[i - 1] + fib[i - 2]) % FLOATING_LAND for i in range(2, n + 1))
+    return fib
 
-print(a)
+fib_values = fibonacci(max(d for _, d in b))
+
+for c, d in b:
+    for i2, i3 in enumerate(range(c, d + 1)):
+        a[i3 - 1] += fib_values[i2 + 1]
+
+a = [x % FLOATING_LAND for x in a]
+print(*a)
