158b3bb42b791229c1a38a11ce4c74128263d54a
misilelab <misileminecord@gmail.com>
2023-06-17 12:13:34 +0900

    final production

--- projects/DML/main.py
+++ projects/DML/main.py
@@ -25,7 +25,7 @@ async def on_ready():
     for i in listdir("modules"):
         if isfile(f"modules/{i}") and i.endswith(".py") and i not in block_list:
             print(f"loading {i}")
-            bot.load_extension(f"modules/{i}")
+            bot.load_extension(f"modules.{i.removesuffix('.py')}")
     print("bot ready")
 
 @bot.event
@@ -45,20 +45,19 @@ async def ping(ctx: ApplicationCommandInteraction):
 @bot.slash_command(name="load", guild_ids=test_ids)
 @is_owner()
 async def load(ctx: ApplicationCommandInteraction, module: str):
-    bot.load_extension(f"modules/{module}.py")
+    bot.load_extension(f"modules/{module}")
     await ctx.send(f"loaded {module}")
 
 @bot.slash_command(name="unload", guild_ids=test_ids)
 @is_owner()
 async def unload(ctx: ApplicationCommandInteraction, module: str):
-    bot.unload_extension(f"modules/{module}.py")
+    bot.unload_extension(f"modules/{module}")
     await ctx.send(f"unloaded {module}")
 
 @bot.slash_command(name="reload", guild_ids=test_ids)
 @is_owner()
 async def reload(ctx: ApplicationCommandInteraction, module: str):
-    bot.reload_extension(f"modules/{module}.py")
+    bot.reload_extension(f"modules/{module}")
     await ctx.send(f"reloaded {module}")
 
-bot.i18n.load('locales')
 bot.run(config["TOKEN"])
--- projects/DML/modules/scpsl.py
+++ projects/DML/modules/scpsl.py
@@ -3,30 +3,36 @@ from disnake import ApplicationCommandInteraction, Embed
 from tomli import load
 from requests import get
 
-config = load(open("config.toml", "r"))
+config = load(open("config.toml", "rb"))
 api_key = config["scpsl_api_key"]
 acc_id = config["scpsl_acc_id"]
+
+def backend_of_scpsl():
+    a = get("https://api.scpslgame.com/serverinfo.php", params={
+        "id": acc_id,
+        "key": api_key,
+        "players": True,
+        "online": True
+    })
+    a.raise_for_status()
+    return a.json()
 
 class SCPSL(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-
-    @staticmethod()
-    def backend_of_scpsl():
-        a = get("https://api.scpslgame.com/serverinfo.php", params={
-            "id": acc_id,
-            "key": api_key,
-            "players": True,
-            "online": True
-        })
-        a.raise_for_status()
-        return a.json()
     
     @slash_command(name="list", description="SCP: SL 서버의 리스트를 보여줍니다")
     async def scpsl_list(self, ctx: ApplicationCommandInteraction):
-        a = self.backend_of_scpsl()
-        embed = Embed(title="SCP: SL list", description=f"현재 {len(i for i in a if i['online'])} 개의 서버가 온라인입니다.", color=0x00ff00)
+        a = backend_of_scpsl()
+        if a["Success"] == False:
+            await ctx.send("Failed", ephemeral=True)
+            return
+        a = a["Servers"]
+        embed = Embed(title="SCP: SL list", description=f"현재 {len([i for i in a if i['Online']])} 개의 서버가 온라인입니다.", color=0x00ff00)
         for i in a:
-            embed.add_field(name=f"{i['name']}-{'online' if i['online'] else 'offline'}", value=f"현재 {i['players']}명이 접속중입니다.", inline=False)
+            embed.add_field(name=f"{i['ID']}-{'online' if i['Online'] else 'offline'}", value=f"현재 {i['Players']}명이 접속중입니다.", inline=False)
         embed.set_author(name="Misile", url="https://github.com/misilelab", icon_url="https://avatars.githubusercontent.com/u/74066467")
         await ctx.send(embed=embed)
+
+def setup(self: Bot):
+    self.add_cog(SCPSL(self))
