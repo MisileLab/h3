68dee5fb5c63b1899a3139fae8d10ccfc73c7df4
misilelab <misileminecord@gmail.com>
2023-06-19 00:09:05 +0900

    fly again

---
+++ projects/ap/bojs/28235.py
@@ -0,0 +1,6 @@
+print({
+    "SONGDO": "HIGHSCHOOL",
+    "CODE": "MASTER",
+    "2023": "0611",
+    "ALGORITHM": "CONTEST"
+}[input()])
---
+++ projects/ap/bojs/2903.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = 2
+
+while a != 0:
+    b += (b - 1)
+    a -= 1
+
+print(b**2)
