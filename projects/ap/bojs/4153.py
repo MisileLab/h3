1013e53413666e0d3311762f8697e2442079653b
misilelab <misileminecord@gmail.com>
2023-06-26 20:21:07 +0900

    asdf bruh

---
+++ projects/ap/bojs/4153.py
@@ -0,0 +1,13 @@
+a = []
+
+while True:
+    b = input().split()
+    if b == ["0", "0", "0"]:
+        break
+    a.append(sorted(list(map(int, b))))
+
+for i in a:
+    if i[0]**2 + i[1]**2 == i[2]**2:
+        print("right")
+    else:
+        print("wrong")
---
+++ projects/ap/dhs/chall869.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from string import printable
+from subprocess import run
+a = Path('flag.enc').read_text().split("0X")[1:]
+c = ""
+d = 0
+
+while True:
+    for i in printable:
+        Path("flag.txt").write_text(c + i)
+        run("./prob")
+        b = Path('tmp.enc').read_text().split("0X")[1:]
+        if a[d] == b[d]:
+            c += i
+            d += 1
+            print(c)
+            break
