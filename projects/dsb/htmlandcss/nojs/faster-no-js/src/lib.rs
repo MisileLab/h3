822db0118ae27b4de51a6662b906606251ab1bf5
misilelab <misileminecord@gmail.com>
2023-06-30 13:45:26 +0900

    no unmaintained && snyk test error

--- projects/dsb/htmlandcss/nojs/faster-no-js/src/lib.rs
+++ projects/dsb/htmlandcss/nojs/faster-no-js/src/lib.rs
@@ -1,9 +1,6 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
