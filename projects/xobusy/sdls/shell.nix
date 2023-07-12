fc6bcb954e4024d79f4b6ca99d70377193b36ce5
MisileLab <misileminecord@gmail.com>
2023-07-13 02:05:17 +0900

    experimental nix
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ projects/xobusy/sdls/shell.nix
@@ -0,0 +1,35 @@
+{
+  description = "SDL Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [];
+    };
+  in {
+    devShells.default = pkgs.mkShell rec {
+      name = "Nix SDL";
+
+      packages = with pkgs; [
+        llvmPackages_16.clang
+        SDL2
+      ];
+
+      shellHook = let
+        icon = "f121";
+      in ''
+        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+        ./start.sh
+      '';
+    };
+
+    packages.default = pkgs.callPackage ./default.nix {};
+  });
+}
