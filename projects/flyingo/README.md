14cbe1efbcd0b9187495ddb5b240211f0ac32dea
MisileLab <misileminecord@gmail.com>
2023-07-13 14:56:37 +0900

    bruh
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ projects/flyingo/README.md
@@ -0,0 +1,3 @@
+# Flyingo
+
+Modified dragonfly bukkit
---
+++ projects/flyingo/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/df-mc/dragonfly/server"
+	"github.com/df-mc/dragonfly/server/player/chat"
+	"github.com/pelletier/go-toml"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func main() {
+	configdir := os.Getenv("config_dir")
+	if configdir == "" {
+		os.Setenv("config_dir", "config.toml")
+	}
+	log := logrus.New()
+	log.Formatter = &logrus.TextFormatter{ForceColors: true}
+	log.Level = logrus.DebugLevel
+
+	chat.Global.Subscribe(chat.StdoutSubscriber{})
+
+	conf, err := readConfig(log)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	srv := conf.New()
+	srv.CloseOnProgramEnd()
+
+	srv.Listen()
+	for srv.Accept(nil) {
+	}
+}
+
+// readConfig reads the configuration from the config.toml file, or creates the
+// file if it does not yet exist.
+func readConfig(log server.Logger) (server.Config, error) {
+	c := server.DefaultConfig()
+	var zero server.Config
+	if _, err := os.Stat(os.Getenv("config_dir")); os.IsNotExist(err) {
+		data, err := toml.Marshal(c)
+		if err != nil {
+			return zero, fmt.Errorf("encode default config: %v", err)
+		}
+		if err := os.WriteFile(os.Getenv("config_dir"), data, 0644); err != nil {
+			return zero, fmt.Errorf("create default config: %v", err)
+		}
+		return c.Config(log)
+	}
+	data, err := os.ReadFile(os.Getenv("config_dir"))
+	if err != nil {
+		return zero, fmt.Errorf("read config: %v", err)
+	}
+	if err := toml.Unmarshal(data, &c); err != nil {
+		return zero, fmt.Errorf("decode config: %v", err)
+	}
+	return c.Config(log)
+}
