aec0c80ce95ad06eb4cf2a6816f0ee7a0a006d27
MisileLab <misileminecord@gmail.com>
2023-07-17 14:56:27 +0900

    bdsx
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/utils/src/main.zig
+++ projects/utils/src/main.zig
@@ -1,17 +1,21 @@
 const std = @import("std");
 const rand = std.rand.DefaultPrng;
+const stringl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 pub fn main() !void {
-    var rnd = rand.init(
-        blk: {
-            var seed: u64 = undefined;
-            try std.os.getrandom(std.mem.asBytes(&seed));
-            break :blk seed;
-        }
-    );
+    var rnd = rand.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+
+    var string: [100]u8 = undefined;
+    for (&string) |*char| {
+        char.* = stringl[rnd.random().intRangeLessThan(u8, 0, stringl.len-1)];
+    }
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    try stdout.print("borg create ./backup::{} ./bdsx\n", .{rnd.random().int(u128)});
+    try stdout.print("borg create ./backup::{s} ./bdsx\n", .{string});
     try bw.flush();
 }
