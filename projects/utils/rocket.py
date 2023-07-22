a8818a366830948a88e41ba702e2b54a82cdb88e
MisileLab <misileminecord@gmail.com>
2023-07-22 19:33:56 +0900

    launch the rocket
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ projects/utils/rocket.py
@@ -0,0 +1,38 @@
+from subprocess import run
+from os import get_terminal_size
+from time import sleep
+
+print(f"{chr(27)}[2J")
+outs = run("git rev-parse --short HEAD", shell=True, capture_output=True, text=True).stdout.removesuffix("\n")
+rocket = rf"""
+          !
+          !
+          ^
+         / \
+        /   \
+       /     \
+      /       \
+     /         \
+    /___________\
+    |=         =|
+    |  {outs}  |
+    |           |
+    |           |
+    |           |
+    |           |
+    |           |
+    |           |
+    |           |
+    |           |
+   /|#####!#####|\
+  / |#####!#####| \
+ /  |#####!#####|  \
+|  /   ^  |  ^  \  |
+| /    (  |  )   \ |
+|/     (  |  )    \|
+"""  # noqa: E501
+
+for i in range(get_terminal_size().lines):
+    print(rocket + "\n" * i)
+    sleep(1)
+    print(f"{chr(27)}[2J")
