48f2d8073e18a257bc6b12417b0a40f783af899d
MisileLab <misileminecord@gmail.com>
2023-07-26 23:11:05 +0900

    boj and sdl end
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/xobusy/sdls/build.sh
+++ projects/xobusy/sdls/build.sh
@@ -1,1 +1,1 @@
-clang -O2 `sdl2-config --libs` -lSDL2_image -I draw.h draw.c -I input.h input.c -I image.h image.c main.c -o main.out
+clang -O2 `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -I draw.h draw.c -I input.h input.c -I image.h image.c main.c -o main.out
--- projects/xobusy/sdls/main.c
+++ projects/xobusy/sdls/main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include "draw.h"
 #include "input.h"
@@ -10,14 +11,13 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-void _repeat(SDL_Renderer* renderer, int x, int y) {
-	SDL_SetRenderDrawColor(renderer, 20, 80, 172, 0xFF);
-	SDL_RenderClear(renderer);
-	// drawFilledCircle(renderer, 320 - 100, 240 - 40, 50, 0, 0, 0, 255);
-	// drawFilledCircle(renderer, 320 + 100, 240 - 40, 50, 0, 0, 0, 255);
-	// drawFilledTriangle(renderer, 320, 240, 320 - 20, 240 + 50, 320 + 20, 240 + 50, 255, 0, 255, 255);
-	drawFilledRectangle(renderer, x, y, 100, 200, 0, 0, 0, 255);
-	SDL_RenderPresent(renderer);
+SDL_Texture* makeText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color color, int x, int y) {
+	SDL_Surface* surface = TTF_RenderText_Solid(font, text, color);
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_Rect rect = { x, y, surface->w, surface->h };
+	SDL_RenderCopy(renderer, texture, NULL, &rect);
+	SDL_FreeSurface(surface);
+	return texture;
 }
 
 int main() {
@@ -51,7 +51,35 @@ int main() {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0xFF);
 	SDL_RenderClear(renderer);
 
-	
+	// Initialize SDL_ttf library
+	if (TTF_Init() == -1) {
+		printf("TTF_Init: %s\n", TTF_GetError());
+		return 1;
+	}
+
+	// Load font file
+	TTF_Font* font = TTF_OpenFont("a.ttf", 28);
+	if (!font) {
+		printf("TTF_OpenFont: %s\n", TTF_GetError());
+		return 1;
+	}
+
+	// Set up text surface and texture
+	SDL_Color textColor = { 255, 255, 255 };
+	SDL_Surface* textSurface = TTF_RenderText_Solid(font, "Hello, I'm a chatbot!", textColor);
+	SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+	// Get text dimensions
+	int textWidth = textSurface->w;
+	int textHeight = textSurface->h;
+
+	// Set text position
+	int textX = 20;
+	int textY = SCREEN_HEIGHT - textHeight - 20;
+
+	// Render text
+	SDL_Rect textRect = { textX, textY, textWidth, textHeight };
+	SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
 
 	SDL_Event event;
 	struct timespec ts = {0, 100};
@@ -70,6 +98,10 @@ int main() {
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	SDL_FreeSurface(textSurface);
+	SDL_DestroyTexture(textTexture);
+	TTF_CloseFont(font);
+	TTF_Quit();
 	SDL_Quit();
 
 	return 0;
