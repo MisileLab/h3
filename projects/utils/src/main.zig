f75e6403bee85ba7cbe439ebf41b7995eb5038ec
MisileLab <misileminecord@gmail.com>
2023-07-27 10:14:56 +0900

    ziggy new support
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/utils/src/main.zig
+++ projects/utils/src/main.zig
@@ -3,6 +3,9 @@ const rand = std.rand.DefaultPrng;
 const stringl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 pub fn main() !void {
+    var args = std.process.args();
+    _ = args.skip();
+    var arg = args.next() orelse "bdsx";
     var rnd = rand.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
@@ -16,6 +19,6 @@ pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    try stdout.print("borg create ./backup::{s} ./bdsx\n", .{string});
+    try stdout.print("borg create ./backup::{s} ./{s}\n", .{string, arg});
     try bw.flush();
 }
