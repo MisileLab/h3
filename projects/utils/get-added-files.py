7826b42cf18b1359ab354bc4c1c49be644d7e9ad
MisileLab <misileminecord@gmail.com>
2023-08-13 02:34:58 +0900

    asdf
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/utils/get-added-files.py
+++ projects/utils/get-added-files.py
@@ -9,7 +9,12 @@ try:
 except Exception:
     a = Repo()
 b = a.head.commit.diff("HEAD^1")
-c = [i.a_path for i in b if i.change_type == "A" and i.a_path.endswith(".jar") and i.a_path.count("servermods") == 0]
+c = [
+    i.a_path for i in b 
+    if i.change_type == "A" and 
+    i.a_path.endswith(".jar") and 
+    i.a_path.count("servermods") == 0
+]
 print(c)
-if c is not None:
+if c:
     run(["7zz", "a", "added.zip", *c], shell=True, check=True)
