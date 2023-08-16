c31b73ad44b2db74de3a1b7fff5976e95830280e
MisileLab <misileminecord@gmail.com>
2023-08-16 10:46:52 +0900

    optimizes util
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/utils/src/main.zig
+++ projects/utils/src/main.zig
@@ -10,22 +10,11 @@ pub fn c_s(a: [:0]const u8, b: [:0] const u8) bool {
 
 pub fn main() !void {
     var args = std.process.args();
-    var exe = args.next() orelse "utils";
-    var command = args.next() orelse "backup";
-    var arg = args.next() orelse "bdsx";
+    _ = args.skip();
+    var command = args.next() orelse "random";
     var bw = io.bufferedWriter(io.getStdOut().writer());
-    var br = io.bufferedReader(io.getStdIn().reader());
     const stdout = bw.writer();
-    const stdin = br.reader();
-    const palloc = std.heap.page_allocator;
-    if (!(c_s(exe, "./utils") or c_s(exe, "utils"))) {
-        const file = try std.fs.cwd().createFile("output.txt", .{ .read = true, .truncate = true });
-        _ = try file.write(command);
-        try bw.flush();
-        var a = try stdin.readAllAlloc(palloc, 10);
-        _ = try stdin.read(a);
-    }
-    else if (c_s(command, "backup")) {
+    if (c_s(command, "random")) {
         var rnd = rand.init(blk: {
             var seed: u64 = undefined;
             try std.os.getrandom(std.mem.asBytes(&seed));
@@ -36,10 +25,10 @@ pub fn main() !void {
         for (&string) |*char| {
             char.* = stringl[rnd.random().intRangeLessThan(u8, 0, stringl.len - 1)];
         }
-        try stdout.print("borg create --progress ./backup::{s} ./{s}\n", .{ string, arg });
+        try stdout.print("{s}\n", .{ string });
         try bw.flush();
     } else {
-        try stdout.print("./utils backup\n", .{});
+        try stdout.print("./utils random\n", .{});
         try bw.flush();
     }
 }
