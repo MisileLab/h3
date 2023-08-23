bfb83b006825a467488203ad37a8bd7968388960
MisileLab <misileminecord@gmail.com>
2023-08-23 10:32:57 +0900

    no node
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/chesscli/main.py
+++ projects/chesscli/main.py
@@ -2,43 +2,54 @@ from chess import Board, Move
 from berserk import Client, TokenSession
 from pathlib import Path
 
-a = Board()
+from time import sleep
+
 b = Path("token.txt").read_text()
 c = Client(session=TokenSession(b))
 name = c.account.get()["username"]
-me = False
 nl = "\n"
-while not a.is_game_over():
-    gstate = c.board.stream_game_state("ccFnwpDbpPN2")
-    me = True
-    for i in gstate:
-        print(me)
-        if i["type"] == "gameFull":
-            if i["white"]["id"] != name:
-                me = False
-                continue
-        elif i["type"] == "gameState" and not me:
-            try:
-                if i["moves"] != "":
-                    a.push(Move.from_uci(i["moves"].split(" ")[-1]))
-            except AssertionError:
-                pass
-        print(f"{chr(27)}[2J")
-        print(f"""{
-            nl.join(f'{8-x2} {x}' for x2, x in enumerate(reversed(str(a).split(nl))))
-        }\n  A B C D E F G H""")
-        if me:
-            while True:
-                e = input(">")
-                m = Move.from_uci(e)
-                print(a.legal_moves)
-                if m in a.legal_moves:
-                    a.push(m)
-                    c.board.make_move("ccFnwpDbpPN2", e)
-                    break
-                print("inv move")
-        print(f"{chr(27)}[2J")
-        print(f"""{
-            nl.join(f'{8-x2} {x}' for x2, x in enumerate(reversed(str(a).split(nl))))
-        }\n  A B C D E F G H""")
-        me = not me
+while True:
+    a = Board()
+    me = False
+    whitename = ""
+    for x in c.board.stream_incoming_events():
+        if x["type"] == "gameStart":
+            print(x["game"]["gameId"])
+            while not a.is_game_over():
+                gstate = c.board.stream_game_state(x["game"]["gameId"])
+                me = True
+                for i in gstate:
+                    print(me)
+                    if i["type"] == "gameFull":
+                        whitename = i["white"]["id"]
+                        if i["white"]["id"] != name:
+                            me = False
+                            continue
+                    elif i["type"] == "gameState" and i["moves"] != "":
+                        print(a)
+                        a.push(Move.from_uci(i["moves"].split(" ")[-1]))
+                    print(f"{chr(27)}[2J")
+                    print(f"""{
+                        nl.join(
+                            f'{8-x2 if whitename == name else x2+1} {x}' 
+                            for x2, x in enumerate(reversed(str(a).split(nl)) 
+                            if whitename != name else str(a).split(nl))
+                        )
+                    }\n  A B C D E F G H""")
+                    if me:
+                        while True:
+                            e = input(">")
+                            m = Move.from_uci(e)
+                            if m in a.legal_moves:
+                                c.board.make_move(x["game"]["gameId"], e)
+                                break
+                            print("inv move")
+                    me = not me
+                print(f"""{
+                nl.join(
+                    f'{8-x2 if whitename == name else x2+1} {x}' 
+                    for x2, x in enumerate(reversed(str(a).split(nl)) 
+                    if whitename != name else str(a).split(nl))
+                )
+                }\n  A B C D E F G H""")
+    sleep(0.1)
