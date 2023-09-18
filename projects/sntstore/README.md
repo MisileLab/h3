61aded311d1c674a1ab0327a3220cd074dae0b84
MisileLab <misileminecord@gmail.com>
2023-09-18 17:13:54 +0900

    bruh
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ projects/sntstore/README.md
@@ -0,0 +1,3 @@
+# sntstore
+
+real sntstore
---
+++ projects/sntstore/src/App.tsx
@@ -0,0 +1,11 @@
+import { createSignal } from "solid-js";
+import { invoke } from "@tauri-apps/api/tauri";
+import styles from './styles.module.css';
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
