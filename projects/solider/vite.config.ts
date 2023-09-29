e2dc84a48d33b12b55961a4a67e60cb11696ed77
MisileLab <misileminecord@gmail.com>
2023-09-29 13:40:57 +0900

    solider remake
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/solider/vite.config.ts
+++ projects/solider/vite.config.ts
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+  plugins: [solid()],
 });
