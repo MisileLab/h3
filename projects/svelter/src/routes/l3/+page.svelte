8ea082fb06fc73371adc4807a5067912c8d9137f
MisileLab <misileminecord@gmail.com>
2023-10-03 01:24:43 +0900

    upgrade
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ projects/svelter/src/routes/l3/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { goto } from '$app/navigation';
+    console.log(history.state);
+    let pw = history.state["password"];
+    if (pw == undefined) {
+        goto("/")
+    }
+</script>
+
+<h1>Welcome to password manager</h1>
+<h2>Password is hidden</h2>
+<p style="display: none;">password is {pw}</p>
+<button type="button" on:click={()=>{goto("/3")}}>goto prev</button>
