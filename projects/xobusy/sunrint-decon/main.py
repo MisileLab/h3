413d4c54ad79a4eeadeb526149f26bad5b9165f6
MisileLab <misileminecord@gmail.com>
2023-10-28 17:05:43 +0900

    some register thing

--- projects/xobusy/sunrint-decon/main.py
+++ projects/xobusy/sunrint-decon/main.py
@@ -10,3 +10,16 @@ db = MongoClient(Path("MONGO_URI").read_text().strip())["local"]["schoolfinder"]
 async def is_valid(name: str, password: str) -> bool:
     return db.find_one({"name":name,"password":password}) != None
 
+@app.post("/register")
+async def register(
+    name: Union[str, None] = Header(default=None),
+    password: Union[str, None] = Header(default=None)
+):
+    if name == None or password == None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    if db.find_one({"name":name}):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+
+    db.insert_one({"name":name,"password":password})
+
