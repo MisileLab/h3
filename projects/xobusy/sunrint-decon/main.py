d0bc0d119a63199622f5f23c5c4958473cfe74f7
MisileLab <misileminecord@gmail.com>
2023-11-05 19:12:28 +0900

    final
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/xobusy/sunrint-decon/main.py
+++ projects/xobusy/sunrint-decon/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from pathlib import Path
+import base64
 
 from fastapi import FastAPI, Header, status, HTTPException
 from pymongo import MongoClient
@@ -10,14 +11,14 @@ config = loads(Path("config.toml").read_text())
 app = FastAPI()
 db = MongoClient(config["MONGO_URI"])["schoolfinder"]
 
-async def is_valid(name: str, password: str) -> bool:
+def is_valid(name: str, password: str) -> bool:
     return db["login"].find_one({"name":name,"password":password}) != None
 
 @app.post("/register")
 async def register(
     name: Union[str, None] = Header(default=None),
     password: Union[str, None] = Header(default=None)
-):
+) -> None:
     if name is None or password is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
@@ -27,14 +28,25 @@ async def register(
     db["login"].insert_one({"name":name,"password":password})
 
 @app.get("/school/search")
-async def school_search(name: str):
-    a = get("https://open.neis.go.kr/hub/schoolinfo",params={"KEY":config["NEIS_KEY"],"TYPE":"json","SCHUL_NM":name})
+async def school_search(name: Union[str, None] = Header(default=None)) -> list[dict]:
+    if name is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    name = base64.b64decode(name).decode("utf-8")
+    a = get("https://open.neis.go.kr/hub/schoolInfo",params={"KEY":config["NEIS_KEY"],"TYPE":"json","SCHUL_NM":name,"SCHUL_KND_SC_NM":"고등학교"})
     a.raise_for_status()
-    return a.json()["schoolInfo"][2]["row"]
+    return [{
+        "name": i["SCHUL_NM"],
+        "kind": i["HS_SC_NM"],
+        "code": i["SD_SCHUL_CODE"],
+        "homepage": i["HMPG_ADRES"],
+        "address": i["ORG_RDNMA"],
+        "callnumber": i["ORG_TELNO"],
+        "created": i["FOND_YMD"]
+    } for i in a.json()["schoolInfo"][1]["row"]]
 
 @app.get("/school/review")
-async def school_review(code: int):
-    return db["info"].find_one({"code":code})
+async def school_review(code: int) -> list[dict]:
+    return list(db["info"].find({"code":code}))[0]["reviews"]
 
 @app.post("/school/review")
 async def school_review_post(
@@ -50,12 +62,14 @@ async def school_review_post(
     if not is_valid(username,password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-    data = db["info"].find_one({"code":code})
+    data = db["info"].find({"code":code})
     if data is None:
         db["info"].insert_one({"code":code,"reviews":[]})
-        data = db["info"].find_one({"code":code})
+        data = db["info"].find({"code":code})
 
-    if [i for i in data if i["username"] == username]:
+    data = list(data)[0]
+
+    if [i for i in data['reviews'] if i["username"] == username]:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT)
 
     data["reviews"].append({"username":username,"review":review,"star":stars})
