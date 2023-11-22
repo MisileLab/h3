c909e3fd458fc1af9c7f4d186b7d3d5c1bc49df4
MisileLab <misileminecord@gmail.com>
2023-11-22 14:21:35 +0900

    final
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/qubit/README.md
+++ projects/qubit/README.md
@@ -1,3 +1,5 @@
+I actually made with vercel v0  
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- projects/qubit/components/main.tsx
+++ projects/qubit/components/main.tsx
@@ -29,15 +29,15 @@ export function Main() {
           <nav className="hidden lg:flex space-x-2">
             <Link
               className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
-              href="#"
+              href="/"
             >
               Home
             </Link>
             <Link
               className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
-              href="#"
+              href="/newScan"
             >
-              Scans
+              New Scan
             </Link>
             {/*<Link
               className="px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
