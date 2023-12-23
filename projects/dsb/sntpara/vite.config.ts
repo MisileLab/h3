c0e90a5af3b4010371df4effab8af3e472926fcf
MisileLab <misileminecord@gmail.com>
2023-12-23 22:27:09 +0900

    sus

--- projects/dsb/sntpara/vite.config.ts
+++ projects/dsb/sntpara/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from 'solid-devtools/vite';
+import compression from 'vite-plugin-compression2';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
     */
     devtools(),
     solidPlugin(),
+    compression()
   ],
   server: {
     port: 3000,
