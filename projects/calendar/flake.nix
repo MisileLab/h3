8f5a1e9f63672c48ec3deee8e03c465765d906ec
MisileLab <misileminecord@gmail.com>
2023-12-31 01:46:29 +0900

    flake fix plus
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/calendar/flake.nix
+++ projects/calendar/flake.nix
@@ -2,26 +2,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        overlays = [
-          rust-overlay.overlays.default
-          (final: prev: {
-            rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          })
-        ];
-        supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-        forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-          pkgs = import nixpkgs { inherit overlays system; };
-        });
+        overlays = [ (import rust-overlay)
+        (final: prev: {
+          rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        })];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 
         libraries = with pkgs; [
           webkitgtk
@@ -35,17 +28,16 @@
       in
       {
         devShell = pkgs.mkShell {
-          packages = with pkgs; [
-          pkg-config
-          dbus
-          openssl
-          glib
-          gtk3
-          libsoup
-          webkitgtk
-          appimagekit
-          nodePackages_latest.pnpm
-          rustToolchain
+          buildInputs = with pkgs; [
+            pkg-config
+            dbus
+            openssl
+            glib
+            gtk3
+            libsoup
+            webkitgtk
+            appimagekit
+            rustToolchain
           ];
 
           shellHook =
