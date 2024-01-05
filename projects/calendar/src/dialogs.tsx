627cfc2ae3237cd4c1bd8c26f513cf7051ff43cc
MisileLab <misileminecord@gmail.com>
2024-01-05 19:50:46 +0900

    some impl
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/calendar/src/dialogs.tsx
+++ projects/calendar/src/dialogs.tsx
@@ -1,62 +1,114 @@
-import { Accessor, JSX, Setter } from "solid-js";
-import { SimpleEvent } from "./interfaces";
+import { Accessor, JSX, Setter, createEffect, createSignal } from "solid-js";
+import { SimpleEvent, Event } from "./interfaces";
 import { AlertDialog, TextField } from "@kobalte/core";
 import { convertDateToString } from "./utils";
 
-export function AlertDialogForEvent(item: SimpleEvent, comp: JSX.Element) {
+export interface tmpfordialog {
+  date: string,
+  time: string
+}
+
+function dialogContent(
+  title: Accessor<string>,
+  setTitle: Setter<string>,
+  start: Accessor<tmpfordialog>,
+  setStart: Setter<tmpfordialog>,
+  end: Accessor<tmpfordialog>,
+  setEnd: Setter<tmpfordialog>,
+  content: Accessor<string>,
+  setContent: Setter<string>
+) {
+  return (<TextField.Root>
+    <div class="w-full h-full flex flex-col ml-1 mr-1 mb-1 mt-1">
+      <TextField.Input
+        class="text-2xl font-bold outline-none bg-transparent dark:text-ctp-text"
+        style="border-radius: 10px;"
+        type="text"
+        size={10}
+        spellcheck={false}
+        value={title()}
+        onChange={(e)=>setTitle(e.target.value)}
+      />
+      <TextField.Label class="w-fit">시작 날짜</TextField.Label>
+      <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
+        <TextField.Input
+          class="outline-none bg-transparent"
+          type="date"
+          value={start()["date"]}
+          onchange={(e)=>setStart({
+            "date": e.target.value,
+            "time": start()["time"]
+          })}
+        />
+        <TextField.Input
+          class="outline-none bg-transparent"
+          type="time"
+          onchange={(e)=>setStart({
+            "date": start()["date"],
+            "time": e.target.value
+          })}
+        />
+      </div>
+      <TextField.Label class="w-fit">끝나는 날짜</TextField.Label>
+      <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
+        <TextField.Input
+          class="outline-none bg-transparent"
+          type="date"
+          value={end()["date"]}
+          onchange={(e)=>setStart({
+            "date": e.target.value,
+            "time": end()["time"]
+          })}
+        />
+        <TextField.Input
+          class="outline-none bg-transparent"
+          type="time"
+          value={end()["time"]}
+          onchange={(e)=>setStart({
+            "date": end()["date"],
+            "time": e.target.value
+          })}
+        />
+      </div>
+      <TextField.Label class="w-fit">설명</TextField.Label>
+      <TextField.TextArea
+        class="bg-transparent outline-none resize-none max-h-24 dark:text-ctp-text scroll-smooth w-fit"
+        spellcheck={false}
+        autoResize
+        value={content()}
+        onchange={(e)=>setContent(e.target.value)}
+      />
+    </div>
+  </TextField.Root>);
+}
+
+export function AlertDialogForEvent(item: SimpleEvent, comp: JSX.Element, events: Event[]) {
   const o = convertDateToString(item.org.start, item.org.end);
+  let first = false;
+  const [open, setOpen] = createSignal(false);
+  const [title, setTitle] = createSignal(item.title);
+  const [start, setStart] = createSignal({"date": o.start.date, "time": o.start.time});
+  const [end, setEnd] = createSignal({"date": o.end.date, "time": o.end.time});
+  const [content, setContent] = createSignal(item.content);
+  createEffect(async () => {
+    if (!open() && !first) {
+      events.push({
+        "start": start(),
+        "title": title(),
+        "content": content(),
+        "end": end(),
+        "color": "#0xc0ffee"
+      })
+    }
+  })
   return (
-    <AlertDialog.Root>
+    <AlertDialog.Root open={open()} onOpenChange={(b)=>{setOpen(b);first=true;}}>
       <AlertDialog.Trigger>{comp}</AlertDialog.Trigger>
       <AlertDialog.Portal>
         <AlertDialog.Overlay />
         <div class="flex fixed inset-0 z-50 items-center justify-center overlay w-full h-full bg-black bg-opacity-30">
           <AlertDialog.Content class="content glass bg-white dark:bg-ctp-overlay0">
-            <TextField.Root>
-              <div class="w-full h-full flex flex-col ml-1 mr-1 mb-1 mt-1">
-                <TextField.Input
-                  class="text-2xl font-bold outline-none bg-transparent dark:text-ctp-text"
-                  style="border-radius: 10px;"
-                  type="text"
-                  size={10}
-                  spellcheck={false}
-                  value={item.title}
-                />
-                <TextField.Label class="w-fit">시작 날짜</TextField.Label>
-                <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
-                  <TextField.Input
-                    class="outline-none bg-transparent"
-                    type="date"
-                    value={o["start"]["date"]}
-                  />
-                  <TextField.Input
-                    class="outline-none bg-transparent"
-                    type="time"
-                    value={o["start"]["time"]}
-                  />
-                </div>
-                <TextField.Label class="w-fit">끝나는 날짜</TextField.Label>
-                <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
-                  <TextField.Input
-                    class="outline-none bg-transparent"
-                    type="date"
-                    value={o["end"]["date"]}
-                  />
-                  <TextField.Input
-                    class="outline-none bg-transparent"
-                    type="time"
-                    value={o["end"]["time"]}
-                  />
-                </div>
-                <TextField.Label class="w-fit">설명</TextField.Label>
-                <TextField.TextArea
-                  class="bg-transparent outline-none resize-none max-h-24 dark:text-ctp-text scroll-smooth w-fit"
-                  spellcheck={false}
-                  autoResize
-                  value={item.content}
-                />
-              </div>
-            </TextField.Root>
+            {dialogContent(title, setTitle, start, setStart, end, setEnd, content, setContent)}
           </AlertDialog.Content>
         </div>
       </AlertDialog.Portal>
@@ -68,6 +120,10 @@ export function CreateEventDialog(
   modal: Accessor<boolean>,
   setModalVisible: Setter<boolean>
 ) {
+  const [title, setTitle] = createSignal("");
+  const [start, setStart] = createSignal({"date": "", "time": ""});
+  const [end, setEnd] = createSignal({"date": "", "time": ""});
+  const [content, setContent] = createSignal("");
   return (
     <div
       class="flex fixed inset-0 z-50 items-center justify-center overlay w-full h-full bg-black bg-opacity-30 tmp"
@@ -79,49 +135,12 @@ export function CreateEventDialog(
       <div
         class={`content glass bg-white dark:bg-ctp-overlay0`}
         data-expanded={modal()}
-        onclick={(e)=>{e.stopImmediatePropagation();}}
+        onclick={(e) => {
+          e.stopImmediatePropagation();
+        }}
       >
-        <TextField.Root>
-          <div class="w-full h-full flex flex-col ml-1 mr-1 mb-1 mt-1">
-            <TextField.Input
-              class="text-2xl font-bold outline-none bg-transparent dark:text-ctp-text"
-              style="border-radius: 10px;"
-              type="text"
-              size={10}
-              spellcheck={false}
-            />
-            <TextField.Label class="w-fit">시작 날짜</TextField.Label>
-            <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
-              <TextField.Input
-                class="outline-none bg-transparent"
-                type="date"
-              />
-              <TextField.Input
-                class="outline-none bg-transparent"
-                type="time"
-              />
-            </div>
-            <TextField.Label class="w-fit">끝나는 날짜</TextField.Label>
-            <div class="text-xl text-gray-500 flex flex-row dark:text-ctp-subtext0 w-fit">
-              <TextField.Input
-                class="outline-none bg-transparent"
-                type="date"
-              />
-              <TextField.Input
-                class="outline-none bg-transparent"
-                type="time"
-              />
-            </div>
-            <TextField.Label class="w-fit">설명</TextField.Label>
-            <TextField.TextArea
-              class="bg-transparent outline-none resize-none max-h-24 dark:text-ctp-text scroll-smooth w-fit"
-              spellcheck={false}
-              autoResize
-            />
-          </div>
-        </TextField.Root>
+        {dialogContent(title, setTitle, start, setStart, end, setEnd, content, setContent)}
       </div>
     </div>
   );
 }
-
