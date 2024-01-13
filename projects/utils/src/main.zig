5db17b678b229b3b1bd3548196084f4642713e08
MisileLab <misileminecord@gmail.com>
2024-01-13 20:50:15 +0900

    idk
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/utils/src/main.zig
+++ projects/utils/src/main.zig
@@ -4,14 +4,14 @@ const io = std.io;
 const stringl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 // shortcut for comparing two string
-pub fn c_s(a: [:0]const u8, b: [:0] const u8) bool {
+pub fn c_s(a: [:0]const u8, b: [:0]const u8) bool {
     return std.mem.eql(u8, a, b);
 }
 
 pub fn main() !void {
     var args = std.process.args();
     _ = args.skip();
-    var command = args.next() orelse "random";
+    const command = args.next() orelse "random";
     var bw = io.bufferedWriter(io.getStdOut().writer());
     const stdout = bw.writer();
     if (c_s(command, "random")) {
@@ -25,7 +25,7 @@ pub fn main() !void {
         for (&string) |*char| {
             char.* = stringl[rnd.random().intRangeLessThan(u8, 0, stringl.len - 1)];
         }
-        try stdout.print("{s}\n", .{ string });
+        try stdout.print("{s}\n", .{string});
     } else {
         try stdout.print("./utils random\n", .{});
     }
