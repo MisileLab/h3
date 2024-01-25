2a19fcc76947a80e6e5b3358b0066b000349e856
MisileLab <misileminecord@gmail.com>
2024-01-25 23:10:04 +0900

    website++
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- projects/dsb/simplesite/src/App.tsx
+++ projects/dsb/simplesite/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
         <p>Your ID: {auth0.user?.name}</p>
         <p>Your Secret ID: {auth0.user?.email}</p>
         <p>Your Token: {token}</p>
-        <button onClick={()=>auth0.logout()}>logout</button>
+        <button onClick={()=>auth0.logout({logoutParams: {returnTo: window.location.origin}})}>logout</button>
       </div>}
     </div>
   );
