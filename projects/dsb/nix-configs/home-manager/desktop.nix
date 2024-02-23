2f6602b08472ebdbcd0ffe495381fa4efe7f4555
misilelaboratory <misileminecord@gmail.com>
2024-02-24 02:34:45 +0900

    testing glass
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/desktop.nix
+++ projects/dsb/nix-configs/home-manager/desktop.nix
@@ -24,6 +24,13 @@ in
       }];
       style = ''
         @import "${custom-ctps.waybar}/themes/mocha.css";
+        window#waybar {
+          background: rgba(108, 112, 134, 0.21);
+          border-radius: 16px;
+          box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+          backdrop-filter: blur(5px);
+          border: 1px solid rgba(108, 112, 134, 0.3);
+        }
       '';
     };
   };
