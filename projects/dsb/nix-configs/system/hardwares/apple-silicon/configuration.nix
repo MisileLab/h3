19d3e6a04bd0e19c985daf60be633698ee7bb75c
misilelab <misileminecord@gmail.com>
2024-02-24 10:29:13 +0900

    why apple silicon sus
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/hardwares/apple-silicon/configuration.nix
+++ projects/dsb/nix-configs/system/hardwares/apple-silicon/configuration.nix
@@ -7,8 +7,12 @@
       ./hardware-configuration.nix
     ];
   boot.loader.efi.canTouchEfiVariables = false;
-  hardware.asahi.useExperimentalGPUDriver = true;
-  hardware.asahi.withRust = true;
+  hardware.asahi = {
+    withRust = true;
+    useExperimentalGPUDriver = true;
+    addEdgeKernelConfig = true;
+    experimentalGPUInstallMode = "replace";
+  };
 
   services.actkbd={enable=true;bindings=[{keys=[225];events=["key"];command="/run/current-system/sw/bin/light -A 10";} {keys=[224];events=["key"];command="/run/current-system/sw/bin/light -U 10";}];};
   programs.light.enable = true;
