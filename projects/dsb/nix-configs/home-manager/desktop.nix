d0c72443a64478b1ec830690697e4133d68d2544
misilelaboratory <misileminecord@gmail.com>
2024-02-24 20:50:58 +0900

    sus
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/desktop.nix
+++ projects/dsb/nix-configs/home-manager/desktop.nix
@@ -3,12 +3,12 @@ let
   custom-ctps = {
     waybar = builtins.fetchGit{url="https://github.com/catppuccin/waybar.git";rev="f74ab1eecf2dcaf22569b396eed53b2b2fbe8aff";};
   };
-  returnColorCSS = {r, g, b, a, additional ? ""}: ''
+  returnColorCSS = {r, g, b, a, addi ? ""}: ''
     /*backdrop-filter: blur(5px);*/
     background: rgba(${r}, ${g}, ${b}, ${a});
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     border-radius: 0;
-    ${additional}
+    ${addi}
   '';
 in
 {
@@ -44,7 +44,7 @@ in
         * {
           font-family: 'Fira Code', monospace;
         }
-        window#waybar {${returnColorCSS(108 112 134 0.4)}}
+        window#waybar {${returnColorCSS({r=108;g=112;b=134;a=0.4;})}}
         #workspaces button {
           ${returnColorCSS(108 112 134 0.4 ''
             padding-left: 6px;
@@ -52,16 +52,16 @@ in
           '')}
         }
         #workspaces button.active {
-          ${returnColorCSS(147 153 178 0.6)}
+          ${returnColorCSS({r=147;g=153;b=178;a=0.6;})}
         }
         #workspace button.urgent {
           background: rgba(249, 226, 175, 0.6);
         }
         #workspace button.hover {
-          ${returnColorCSS(127 132 156 0.6 ''
+          ${returnColorCSS({r=127;g=132;b=156;a=0.6;addi=''
             padding-left: 6px;
             padding-right: 6px;
-          '')}
+          '';})}
         }
         .modules-left * {
           text-color: @subtext1;
@@ -70,11 +70,11 @@ in
           text-color: @text;
         }
         .modules-right * {
-          ${returnColorCSS(147 153 178 0.6 ''
+          ${returnColorCSS({r=147;g=153;b=178;a=0.6;addi=''
             padding-left: 6px;
             padding-right: 6px;
             text-color: @subtext1;
-          '')};
+          '';})};
         }
         #battery.warning {
           background: rgba(249, 226, 175, 0.6);
