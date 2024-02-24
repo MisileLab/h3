8f3eceb34a7df59392f1422b79c0ca74bdc64550
misilelaboratory <misileminecord@gmail.com>
2024-02-24 20:36:13 +0900

    css moment
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/desktop.nix
+++ projects/dsb/nix-configs/home-manager/desktop.nix
@@ -44,21 +44,21 @@ in
         * {
           font-family: 'Fira Code', monospace;
         }
-        window#waybar {${returnColorCss(108, 112, 134, 0.4)}}
+        window#waybar {${returnColorCss(108 112 134 0.4)}}
         #workspaces button {
-          ${returnColorCss(108, 112, 134, 0.4, ''
+          ${returnColorCss(108 112 134 0.4 ''
             padding-left: 6px;
             padding-right: 6px;
           '')}
         }
         #workspaces button.active {
-          ${returnColorCSS(147, 153, 178, 0.6)}
+          ${returnColorCSS(147 153 178 0.6)}
         }
         #workspace button.urgent {
           background: rgba(249, 226, 175, 0.6);
         }
         #workspace button.hover {
-          ${returnColorCss(127, 132, 156, 0.6, ''
+          ${returnColorCss(127 132 156 0.6 ''
             padding-left: 6px;
             padding-right: 6px;
           '')}
@@ -70,11 +70,11 @@ in
           text-color: @text;
         }
         .modules-right * {
-          ${returnColorCss(147, 153, 178, 0.6), ''
+          ${returnColorCss(147 153 178 0.6 ''
             padding-left: 6px;
             padding-right: 6px;
             text-color: @subtext1;
-          ''};
+          '')};
         }
         #battery.warning {
           background: rgba(249, 226, 175, 0.6);
