3a55ecd0672ebb912f2775acc51cb8ad3837c5e9
misilelab <misileminecord@gmail.com>
2024-03-22 14:56:44 +0900

    add docker compose to dhs
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xobusy/dhs/shell.nix
+++ projects/xobusy/dhs/shell.nix
@@ -8,7 +8,7 @@ with import <nixpkgs> {};
   beautifulsoup4 lxml loguru flask]);
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc
-    libseccomp
+    libseccomp docker-compose
   ];
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   }
