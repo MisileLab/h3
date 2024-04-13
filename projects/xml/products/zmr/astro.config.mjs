70f9cfe256e2dce93aad660d3b65ba65abb7a99a
misilelaboratory <misileminecord@gmail.com>
2024-04-13 20:39:07 +0900

    lets start one of post
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/xml/products/zmr/astro.config.mjs
+++ projects/xml/products/zmr/astro.config.mjs
@@ -1,8 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx({
+    shikiConfig: {
+      theme: "catppuccin-mocha",
+      wrap: true
+    }
+  })],
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-mocha",
+      wrap: true
+    }
+  },
+  redirects: {
+    "/": "/pages/1",
+    "/pages": "/pages/1",
+    "/posts": "/pages/1"
+  }
 });
