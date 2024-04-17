4e4fb00460f9c9c655b6ecc23b26a3d240e519e5
misilelab <misileminecord@gmail.com>
2024-04-17 16:45:01 +0900

    perfect backend
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cocktail/backend/main.py
+++ projects/dsb/cocktail/backend/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, HTTPException, status, WebSocket
+from orjson import dumps
+
+from os import SystemRandom
+from pathlib import Path
 
 app = FastAPI()
 CHECKPOINT = "it_needs_to_be_changed"
 VERSION = "Midori Sour"
 websockets = []
+chats = []
 CHECKPOINT = "test"
 
 class User:
  def __init__(self, gpg: str, name: str, ws: WebSocket):
   self.ws = ws
+  self.name = name
+  self.gpg = gpg
 
 if CHECKPOINT == "it_needs_to_be_changed":
  print("plz return")
@@ -23,11 +30,21 @@ async def chat(ws: WebSocket, PW: str):
  if PW != CHECKPOINT:
   raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
  await ws.accept()
- while True:
-  data = await ws.receive_json()
-  if data["type"] == "gpg":
-   websockets.append(User(data["gpg"], data["name"], ws))
-  elif data["type"] == "send":
-   for i in websockets:
-    i.ws.send_json({"msg": data["msg"], "gpg": data["gpg"], "name": data["name"]})
+ try:
+  while True:
+   data = await ws.receive_json()
+   if data["type"] == "gpg":
+    websockets.append(User(data["gpg"], data["name"], ws))
+   elif data["type"] == "send":
+    for i in websockets:
+     i.ws.send_json({"msg": data["msg"], "name": data["name"]})
+     chats.append({"msg": data["msg"], "name": data["name"]})
+ except WebsocketDisconnect:
+  websockets.remove(ws)
+
+ @app.get("/save/{PW}")
+ async def save(PW: str):
+  if PW != CHECKPOINT:
+   raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+  Path(f"/persist/chats/data_{SystemRandom().randint(1, 100_00_000_00000)}").write_text(chats)
 
