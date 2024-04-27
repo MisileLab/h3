5a1a723dca45a011d1a749bca6ca90477d5632f5
misilelab <misileminecord@gmail.com>
2024-04-28 01:22:53 +0900

    add sitemap and more general solution
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xml/products/zmr/astro.config.mjs
+++ projects/xml/products/zmr/astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import node from "@astrojs/node";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blog.misile.xyz",
   integrations: [tailwind(), mdx({
     shikiConfig: {
       theme: "catppuccin-mocha",
       wrap: true
     }
-  })],
+  }), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha",
