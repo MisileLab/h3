3d85e08b11a0659edbffd315ff12dfcf18bc2333
misilelab <misileminecord@gmail.com>
2024-05-09 09:24:13 +0900

    upgrade 5.2 to 5.3
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/sway.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/sway.nix
@@ -84,38 +84,7 @@ in
     };
     swaylock = {
       enable = true;
-      settings = {
-        # https://github.com/catppuccin/swaylock
-        color = "1e1e2e";
-        bs-hl-color = "f5e0dc";
-        caps-lock-bs-hl-color = "f5e0dc";
-        caps-lock-key-hl-color = "a6e3a1";
-        inside-color = "00000000";
-        inside-clear-color = "00000000";
-        inside-caps-lock-color = "00000000";
-        inside-ver-color = "00000000";
-        inside-wrong-color = "00000000";
-        key-hl-color = "a6e3a1";
-        layout-bg-color = "00000000";
-        layout-border-color = "00000000";
-        layout-text-color = "cdd6f4";
-        line-color = "00000000";
-        line-clear-color = "00000000";
-        line-caps-lock-color = "00000000";
-        line-ver-color = "00000000";
-        line-wrong-color = "00000000";
-        ring-color = "b4befe";
-        ring-clear-color = "f5e0dc";
-        ring-caps-lock-color = "fab387";
-        ring-ver-color = "89b4fa";
-        ring-wrong-color = "eba0ac";
-        separator-color = "00000000";
-        text-color = "cdd6f4";
-        text-clear-color = "f5e0dc";
-        text-caps-lock-color = "fab387";
-        text-ver-color = "89b4fa";
-        text-wrong-color = "eba0ac";
-      };
+      catppuccin.enable = true;
     };
     waybar = {
       enable = true;
@@ -187,8 +156,8 @@ in
       window#waybar {${returnColorCSS({r=108;g=112;b=134;a=0.4;})}}
       #workspaces button {
         ${returnColorCSS({r=108;g=112;b=134;a=0.4;addi=''
-        padding-left: 6px;
-        padding-right: 6px;
+        padding-left: 2px;
+        padding-right: 2px;
         '';})}
       }
       #workspaces button.active {
@@ -199,8 +168,8 @@ in
       }
       #workspace button.hover {
         ${returnColorCSS({r=127;g=132;b=156;a=0.6;addi=''
-        padding-left: 6px;
-        padding-right: 6px;
+        padding-left: 2px;
+        padding-right: 2px;
         '';})}
       }
       .modules-left * {
