831d46014eded2aaf78a617f9d69808de85001ef
misilelaboratory <misileminecord@gmail.com>
2024-06-02 04:05:58 +0900

    change to astrojs

--- projects/xml/services/schale/frontend/src/config.ts
+++ projects/xml/services/schale/frontend/src/config.ts
@@ -2,7 +2,8 @@ export interface Data {
   status: number
 }
 
-export const backendurl = import.meta.env.PROD ? (!location.hostname.includes(".onion") ? "https://schale.misile.xyz" : "http://zcif36gyercekeitpfb24e7hrzstzwzbhxh22shcn2emsv2vhaf6cdad.onion") : "http://127.0.0.1:10001"
+import.meta.env.PROD = true;
+export const backendurl = import.meta.env.PROD ? (location.hostname.includes(".onion") ? "http://zcif36gyercekeitpfb24e7hrzstzwzbhxh22shcn2emsv2vhaf6cdad.onion" : "https://schale.misile.xyz") : "http://127.0.0.1:10001"
 export const host = location.protocol + "//" + location.hostname;
 
 export default function statusCheck(r: Data) {
@@ -15,4 +16,3 @@ export default function statusCheck(r: Data) {
   }
   return false;
 }
-
