53672abacbd235382cab10a5635147e0909c2821
misilelaboratory <misileminecord@gmail.com>
2024-06-21 01:15:32 +0900

    ppt fast
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

---
+++ projects/dsb/slucnh/tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require("@catppuccin/tailwindcss")({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "ctp",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "latte",
+    })],
+}
