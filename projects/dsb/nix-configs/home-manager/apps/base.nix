641be4a2e5f5d3d1c3b685efd7a16a162c5b9785
misilelab <misileminecord@gmail.com>
2024-06-26 16:09:48 +0900

    add glance
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/base.nix
@@ -9,7 +9,7 @@
   ];
   home.packages = with pkgs; [
     sbctl bluez cryptsetup smartmontools borgbackup rclone pulsemixer
-    portablemc miniserve openssl tribler transmission
+    portablemc miniserve openssl tribler transmission glances glance
     (pkgs.writeShellScriptBin "manual" ''
       ${pkgs.glow}/bin/glow -p ~/.config/home-manager/manual.md
     '')
