a273224a5de1f2a37db520e896963fc111d6ffdc
misilelab <misileminecord@gmail.com>
2024-07-05 02:31:35 +0900

    add nethogs
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/network.nix
+++ projects/dsb/nix-configs/home-manager/apps/network.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     dhcpcd cloudflare-warp trayscale
+    nethogs (pkgs.writeShellScriptBin "nhs" "sudo ${pkgs.nethogs}/bin/nethogs -b wg0-mullvad")
   ];
   programs = {
     irssi.enable = true;
