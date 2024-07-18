c0cc945da8aec1d7c3b66f715d7c211848856b69
misilelab <misileminecord@gmail.com>
2024-07-18 09:45:04 +0900

    update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -1,4 +1,4 @@
-{c, config, pkgs, ...}:
+{c, config, pkgs, stablep, ...}:
 let
   briar-desktop = pkgs.callPackage ./briar.nix {};
   exodus = pkgs.callPackage ./exodus.nix {};
@@ -17,11 +17,11 @@ in
     packages = with pkgs; [
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick virt-manager gimp appflowy xfce.thunar
-      /* galaxy-buds-client */ ferium (prismlauncher.override{withWaylandGLFW=true;}) qemu
+      /* galaxy-buds-client */ ferium (prismlauncher.override{withWaylandGLFW=true;})
       seahorse kdePackages.filelight telegram-desktop firefoxpwa
       zed-editor jetbrains.idea-community-bin
       onionshare-gui
-    ] ++ ([briar-desktop exodus]);
+    ] ++ ([briar-desktop exodus]) ++ (with stablep; [qemu]);
     file = {
       ".local/share/PrismLauncher/themes/catppuccin-mocha.zip".source = config.lib.file.mkOutOfStoreSymlink "${builtins.fetchGit {
         url="https://github.com/catppuccin/prismlauncher";
--- projects/dsb/nix-configs/home-manager/apps/development.nix
+++ projects/dsb/nix-configs/home-manager/apps/development.nix
@@ -8,7 +8,7 @@
       # Development
       edgedb d2 pre-commit pijul just dive dvc solana-validator
       # https://github.com/NixOS/nixpkgs/pull/311156
-      snyk ghidra /*pwndbg*/ radicle-node infisical pnpm_9 jetbrains-toolbox
+      snyk /*pwndbg*/ radicle-node infisical pnpm_9 jetbrains-toolbox
       #(pkgs.writeShellScriptBin "gdb" "${pkgs.pwndbg}/bin/pwndbg")
 
       # Language tools
@@ -16,7 +16,7 @@
       rustup cargo-update
       python312Full micromamba pdm mypy ruff-lsp
       nasm
-      tailwindcss-language-server volta
+      tailwindcss-language-server deno volta
       hvm kind2
       clang-tools lldb pkg-config
       niv fh nixpkgs-fmt nix-tree hub
@@ -30,7 +30,7 @@
     ++ (with llvmPackages_latest; [libcxxClang openmp libunwind]) # llvm
     ++ (with nodePackages_latest; [nodejs typescript-language-server svelte-language-server]) # nodejs
     ++ (with python312Packages; [pip virtualenv keyring keyrings-cryptfile python-lsp-server mitmproxy]) # python thing
-    ++ (with stablep; [deno]);
+    ++ (with stablep; [ghidra]);
     file = {
       "non-nixos-things/catppuccin-ghidra".source = config.lib.file.mkOutOfStoreSymlink "${builtins.fetchGit{
         url="https://github.com/StanlsSlav/ghidra";
