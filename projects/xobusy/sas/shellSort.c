5d712e1120bf010bbee173891bbd1da0860dd826
misilelab <misileminecord@gmail.com>
2024-09-10 09:35:06 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xobusy/sas/shellSort.c
+++ projects/xobusy/sas/shellSort.c
@@ -9,19 +9,23 @@
 int list[MAX_SIZE];
 int n;
 
-void swap(TYPE* x, TYPE* y) {TYPE t=*x;*x=*y;*y=t;}
-
 void shellSort(int arr[], int n) {
+  // h를 높이와 똑같이 설정
   int h = n;
+  // h가 1이면 정렬이 완료된 것
   while (h > 1) {
+    // h를 처음에 2로 나눔
     h /= 2;
     if (h % 2 == 0) {h++;}
+    // h의 값과 부분 리스트 개수는 같기 때문에 for문을 사용함
     for (int i = 0; i < h; i++) {
+      // h만큼 떨어진 리스트 생성
       for (int j = i + h; j < n; j += h) {
+        // 부분 리스트를 삽입 정렬함
         int k = arr[j];
         int l = j - h;
         while (l >= i && arr[l] > k) {
-          swap(&arr[l], &arr[l + h]);
+          arr[l+h] = arr[l];
           l -= h;
         }
         arr[l + h] = k;
