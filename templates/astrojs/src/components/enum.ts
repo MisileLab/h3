357f1fbebe3df65c3cde57ba6966f1c7a96be38a
misilelaboratory <misileminecord@gmail.com>
2024-09-13 01:59:39 +0900

    feat: :sparkles: new design :sunglasses:
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

---
+++ templates/astrojs/src/components/enum.ts
@@ -0,0 +1,12 @@
+export enum HoverType {
+  none = "none",
+  hide = "hide",
+  show = "show",
+  reanimate = "reanimate"
+}
+
+export enum AnimationType {
+  rewind = "rewind",
+  normal = "normal",
+  random = "random"
+}
