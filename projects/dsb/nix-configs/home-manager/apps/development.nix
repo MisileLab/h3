132988c02762ab53c95e99ff8594478cd2f7bbfa
misilelab <misileminecord@gmail.com>
2024-10-02 12:15:56 +0900

    feat(news): add new news
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/development.nix
+++ projects/dsb/nix-configs/home-manager/apps/development.nix
@@ -7,7 +7,7 @@
     packages = with pkgs; [
       # Development
       edgedb d2 pre-commit pijul just dive (dvc.override{enableAWS=true;}) solana-validator
-      snyk pwndbg radicle-node infisical pnpm_9 jetbrains-toolbox ghidra poop
+      snyk pwndbg radicle-node infisical pnpm_9 jetbrains-toolbox ghidra poop binsider
       (pkgs.writeShellScriptBin "gdb" "${pkgs.pwndbg}/bin/pwndbg") process-compose
 
       # Language tools
