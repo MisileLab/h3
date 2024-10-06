c8f09835ec2a12b0c4a2e21862d30ceb1bce8894
misilelaboratory <misileminecord@gmail.com>
2024-10-07 00:34:11 +0900

    feat(sas): i don't know what's going on help me
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/xobusy/sas/tlzma.py
+++ projects/xobusy/sas/tlzma.py
@@ -12,6 +12,47 @@ class SlidingWindowedByte:
   def to_str_tuple(self) -> tuple[int, int, int]:
     return (self.distance, self.length, chr(self.value))
 
+class ArithmeticCoding:
+  # [0, 1]로 가정하여 인코딩
+  def encode(self, data: list[SlidingWindowedByte], freq_table: dict): # 나중에 dict 세부 타입 작성 예정
+    # 일정 비율로 나눈 뒤에 뒷 부분에서 작업 진행
+    for char in data:
+      self.high = freq_table[char][1]
+      self.low = freq_table[char][0]
+    # 마지막에 2로 나눔 (아직 이해하지 못함)
+    return (self.low + self.high) / 2
+
+#   def decode(self, encoded_value, freq_table, length):
+#     decoded_data = []
+#     for _ in range(length):
+#       for char, (low_range, high_range) in freq_table.items():
+#         if low_range <= encoded_value < high_range:
+#           decoded_data.append(char)
+#           char_range = high_range - low_range
+#           encoded_value = (encoded_value - low_range) / char_range
+#           break
+#     return ''.join(decoded_data)
+
+# def build_frequency_table(data):
+#   freq_table = {}
+#   total_chars = len(data)
+  
+#   for char in data:
+#     if char in freq_table:
+#       freq_table[char] += 1
+#     else:
+#       freq_table[char] = 1
+
+#   # 빈도 테이블을 (low_range, high_range)의 범위로 변환
+#   freq_table_ranges = {}
+#   low_range = 0.0
+#   for char, freq in sorted(freq_table.items()):
+#     high_range = low_range + (freq / total_chars)
+#     freq_table_ranges[char] = (low_range, high_range)
+#     low_range = high_range
+  
+#   return freq_table_ranges
+
 def find_distance(data: bytes, compare: bytes) -> int:
   queue = []
   first_char = -1
