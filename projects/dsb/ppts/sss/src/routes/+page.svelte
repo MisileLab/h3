9029b19a85f1f6778a65400cf8faba0758edf2c0
misilelaboratory <misileminecord@gmail.com>
2024-10-24 04:04:43 +0900

    feat: understand shamir secret sharing not easy and i want to sleep
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/ppts/sss/src/routes/+page.svelte
+++ projects/dsb/ppts/sss/src/routes/+page.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
-	import { Presentation, Slide, Code, Transition, Action } from '@animotion/core'
+	import { Presentation, Slide, Code, Action } from '@animotion/core'
 
-	let text: HTMLParagraphElement
+	let shamir: HTMLElement
+  let secretKeySharing: HTMLElement
 	let code: ReturnType<typeof Code>
 </script>
 
-<Presentation>
+<!--
+https://www.geeksforgeeks.org/shamirs-secret-sharing-algorithm-cryptography/
+https://medium.com/@keylesstech/a-beginners-guide-to-shamir-s-secret-sharing-e864efbf3648
+https://ehdvudee.tistory.com/27
+-->
+
+<Presentation options={{history: true}}>
+  <div class="text-green-400 text-blue-400 underline" style="display: none;" />
 	<Slide class="h-full place-content-center place-items-center">
-    <h1 class="text-8xl">Shamir's secret key sharing</h1>
-    <Action do={()=>{}} />
+    <h1 class="text-8xl">
+      <span bind:this={shamir}>Shamir</span>
+      's <span bind:this={secretKeySharing}>secret key sharing</span>
+    </h1>
+    <Action do={()=>shamir.classList.add('text-green-400')} undo={()=>shamir.classList.remove('text-green-400')} />
+    <Action do={()=>secretKeySharing.classList.add('text-blue-400')} undo={()=>secretKeySharing.classList.remove('text-blue-400')} />
+  </Slide>
+  <Slide class="place-content-center place-items-center">
+    <div class="h-screen w-full py-20">
+      <img src="/shamirSimple.png" class="h-full" />
+    </div>
   </Slide>
 </Presentation>
