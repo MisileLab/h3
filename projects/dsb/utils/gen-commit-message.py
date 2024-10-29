db8163f8e07e6fbf06317c7cf31f0e56cce039da
misilelab <misileminecord@gmail.com>
2024-10-29 12:06:08 +0900

    feat(utils): pre-define commit from argv
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/utils/gen-commit-message.py
+++ projects/dsb/utils/gen-commit-message.py
@@ -1,24 +1,30 @@
 import inquirer
 
 from subprocess import run
+from sys import argv
+from json import loads, dumps
 
-convtypes = ["feat","fix","docs","style","refactor","perf","test","build","ci","chore","revert"]
+if len(argv) < 2:
+  convtypes = ["feat","fix","docs","style","refactor","perf","test","build","ci","chore","revert"]
 
-questions = [
-  inquirer.List("type", message="Type of commit", choices=convtypes),
-  inquirer.List("cscope", message="Custom scope", choices=["y","n"]),
-  inquirer.Text("cscopecon", message="Content of custom scope", ignore=lambda x: x["cscope"] == "n"),
-  inquirer.Text("shortdesc", message="Short description(must be less than 55 chars)", validate=lambda _, x: len(x) <= 55),
-  inquirer.Editor("longdesc", message="Long description"),
-  inquirer.List("bchanges", message="Breaking changes", choices=["y","n"]),
-  inquirer.Editor("bchangescon", message="Breaking change content", ignore=lambda x: x["bchanges"] == "n")
-]
+  questions = [
+    inquirer.List("type", message="Type of commit", choices=convtypes),
+    inquirer.List("cscope", message="Custom scope", choices=["y","n"]),
+    inquirer.Text("cscopecon", message="Content of custom scope", ignore=lambda x: x["cscope"] == "n"),
+    inquirer.Text("shortdesc", message="Short description(must be less than 55 chars)", validate=lambda _, x: len(x) <= 55),
+    inquirer.Editor("longdesc", message="Long description"),
+    inquirer.List("bchanges", message="Breaking changes", choices=["y","n"]),
+    inquirer.Editor("bchangescon", message="Breaking change content", ignore=lambda x: x["bchanges"] == "n")
+  ]
 
-answers = inquirer.prompt(questions)
-print(answers)
+  answers = inquirer.prompt(questions)
 
-if answers is None:
-  exit(1)
+  if answers is None:
+    exit(1)
+
+  print(dumps(answers))
+else:
+  answers = loads(' '.join(argv[1:]))
 
 output = answers["type"]
 
