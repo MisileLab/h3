7363e72ee0f1b21f3ef19f08dab1274333283472
misilelab <misileminecord@gmail.com>
2024-11-04 23:14:23 +0900

    feat(nix)!: delete unmaintained --breaking changes-- yubikey-manager-qt has error and it is unmaintained
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/security.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/security.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    yubikey-manager-qt mullvad-vpn pass-wayland tor-browser-bundle-bin
-    monero-gui (python312Packages.mat2)
+    mullvad-vpn pass-wayland tor-browser-bundle-bin monero-gui (python312Packages.mat2)
   ];
 }
