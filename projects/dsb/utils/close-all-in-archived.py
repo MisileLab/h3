bf1be5708d6acfc743280be8b78f89edd3637388
misilelaboratory <misileminecord@gmail.com>
2024-11-07 02:04:33 +0900

    feat(utils): close all issue and pull request on archived repo
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

---
+++ projects/dsb/utils/close-all-in-archived.py
@@ -0,0 +1,24 @@
+from github import Github
+from github.Auth import Token
+
+from pathlib import Path
+
+g = Github(auth=Token(Path("token.txt").read_text()))
+
+for i in g.get_user().get_repos():
+  print(i, i.archived)
+  if not i.archived:
+    continue
+  issues = i.get_issues(state="open")
+  if issues.totalCount == 0:
+    continue
+  pr = i.get_pulls(state="open")
+  if i.archived and (issues.totalCount == 0 or pr.totalCount == 0):
+    i.edit(archived=False)
+    for j in issues:
+      print(f"closing issue {j.id}")
+      j.edit(state="closed")
+    for j in pr:
+      print(f"closing pr {j.id}")
+      j.edit(state="closed")
+    i.edit(archived=True)
