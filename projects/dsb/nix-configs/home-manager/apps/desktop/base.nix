b5f20add85369538fed9eee45d622ada4fef7739
misilelab <misileminecord@gmail.com>
2024-11-07 09:12:57 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -1,4 +1,4 @@
-{c, config, pkgs, stablep, ...}:
+{c, config, pkgs, ...}:
 let
   briar-desktop = pkgs.callPackage ./briar.nix {};
 in
@@ -16,8 +16,8 @@ in
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick virt-manager appflowy xfce.thunar
       /* galaxy-buds-client */ ferium prismlauncher
-      seahorse kdePackages.filelight (stablep.qemu) firefoxpwa gparted exodus
-      (stablep.onionshare) jetbrains.idea-community-bin gimp zed-editor (stablep.telegram-desktop)
+      seahorse kdePackages.filelight qemu firefoxpwa gparted exodus
+      onionshare jetbrains.idea-community-bin gimp zed-editor telegram-desktop
     ] ++ ([briar-desktop exodus]);
     file = {
       ".local/share/PrismLauncher/themes/catppuccin-mocha.zip".source = config.lib.file.mkOutOfStoreSymlink "${builtins.fetchGit {
