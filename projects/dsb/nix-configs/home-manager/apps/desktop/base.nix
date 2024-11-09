3b20c550c74223e8c737efac676268b7079374ad
misilelab <misileminecord@gmail.com>
2024-11-09 15:57:36 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -1,4 +1,4 @@
-{c, config, pkgs, ...}:
+{c, config, pkgs, stablep, ...}:
 let
   briar-desktop = pkgs.callPackage ./briar.nix {};
 in
@@ -11,13 +11,12 @@ in
     ./sway.nix
   ];
   home = {
-    # https://github.com/NixOS/nixpkgs/issues/306670
     packages = with pkgs; [
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick virt-manager appflowy xfce.thunar
-      /* galaxy-buds-client */ ferium prismlauncher
+      galaxy-buds-client ferium prismlauncher
       seahorse kdePackages.filelight qemu firefoxpwa gparted exodus
-      onionshare jetbrains.idea-community-bin gimp zed-editor telegram-desktop
+      onionshare jetbrains.idea-community-bin gimp (stablep.zed-editor) (stablep.telegram-desktop)
     ] ++ ([briar-desktop exodus]);
     file = {
       ".local/share/PrismLauncher/themes/catppuccin-mocha.zip".source = config.lib.file.mkOutOfStoreSymlink "${builtins.fetchGit {
@@ -54,7 +53,6 @@ in
   };
   xdg = {
     enable = true;
-    # https://github.com/NixOS/nixpkgs/issues/354510
     portal = {
       enable = true;
       extraPortals = with pkgs; [xdg-desktop-portal-gtk xdg-desktop-portal-wlr];
