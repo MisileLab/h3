9fe7101de1b73cdc3eab02d4b2b5bf82b2848f60
misilelab <misileminecord@gmail.com>
2024-11-16 13:10:35 +0900

    feat(nix): new idle status that doesn't misbehave
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/idle-handler.sh
+++ projects/dsb/nix-configs/home-manager/idle-handler.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 # Check the current idle_inhibit status
-STATUS=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.inhibit_idle?) | .inhibit_idle')
+STATUS="0"
 
-if [ "$STATUS" == "" ]; then
+if [ -f /tmp/idle_status ]; then
+  STATUS=`cat /tmp/idle_status`
+fi
+
+if [ "$STATUS" == "0" ]; then
     # Enable idle inhibit
     swaymsg "inhibit_idle visible"
     notify-send "Idle Inhibit Enabled"
+    echo "1" > /tmp/idle_status
 else
     # Disable idle inhibit
     swaymsg "inhibit_idle none"
     notify-send "Idle Inhibit Disabled"
+    echo "0" > /tmp/idle_status
 fi
 
