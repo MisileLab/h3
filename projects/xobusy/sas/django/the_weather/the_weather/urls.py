90f2c32b5b137952d7536c105c3478932d0b911f
misilelab <misileminecord@gmail.com>
2024-11-20 08:36:44 +0900

    feat(django): get out
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xobusy/sas/django/the_weather/the_weather/urls.py
+++ projects/xobusy/sas/django/the_weather/the_weather/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ex_form8 project.
+URL configuration for the_weather project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('weather.urls')),
 ]
