345812315b9960dac88cf467b60ba63b70a4688c
misilelab <misileminecord@gmail.com>
2024-11-23 02:07:23 +0900

    fix(nix): now it truly stores on ram
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/idle-handler.sh
+++ projects/dsb/nix-configs/home-manager/idle-handler.sh
@@ -3,19 +3,19 @@
 # Check the current idle_inhibit status
 STATUS="0"
 
-if [ -f /tmp/idle_status ]; then
-  STATUS=`cat /tmp/idle_status`
+if [ -f /dev/shm/idle_status ]; then
+  STATUS=`cat /dev/shm/idle_status`
 fi
 
 if [ "$STATUS" == "0" ]; then
     # Enable idle inhibit
     swaymsg "inhibit_idle visible"
     notify-send "Idle Inhibit Enabled"
-    echo "1" > /tmp/idle_status
+    echo "1" > /dev/shm/idle_status
 else
     # Disable idle inhibit
     swaymsg "inhibit_idle none"
     notify-send "Idle Inhibit Disabled"
-    echo "0" > /tmp/idle_status
+    echo "0" > /dev/shm/idle_status
 fi
 
