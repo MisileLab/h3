7ae4752655f3af3c5003ebf7faa82fd2609eae4f
misilelab <misileminecord@gmail.com>
2024-11-28 23:54:03 +0900

    feat(nix): add bash lsp
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/development.nix
+++ projects/dsb/nix-configs/home-manager/apps/development.nix
@@ -26,7 +26,7 @@
       vala
 
       # lsp
-      shellcheck basedpyright nil vala-language-server
+      shellcheck basedpyright nil vala-language-server bash-language-server
 
       # custom file
       (pkgs.writeShellScriptBin "bs" "infisical run --project-config-dir=/home/misile/repos/h3/projects/dsb/utils -- ${pkgs.uv}/bin/uv run -p ~/repos/h3/projects/dsb/utils ~/repos/h3/projects/dsb/utils/butter-shell.py")
