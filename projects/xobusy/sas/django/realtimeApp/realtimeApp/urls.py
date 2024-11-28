1c5e20dbb8d46de6be578c3eb561f6a1c5eb0299
misilelab <misileminecord@gmail.com>
2024-11-28 23:54:09 +0900

    feat(django): get out
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xobusy/sas/django/realtimeApp/realtimeApp/urls.py
+++ projects/xobusy/sas/django/realtimeApp/realtimeApp/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path("", index)
 ]
