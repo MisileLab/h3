d66162d6e3d751814787350fabde4df72e69380f
misilelab <misileminecord@gmail.com>
2024-11-30 17:50:46 +0900

    feat(nix): add cloc
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/utils.nix
+++ projects/dsb/nix-configs/home-manager/apps/utils.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [
     # file
     axel wget file wgetpaste convmv termscp ouch rhash duperemove
-    fd
+    fd cloc
 
     # some fancy cli tools
     fastfetch onefetch delta genact glow dasel navi nix-output-monitor
