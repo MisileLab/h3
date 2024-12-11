fa2be18e87e1749b28c2831d4cc720350d9956b8
misilelab <misileminecord@gmail.com>
2024-12-12 03:24:30 +0900

    feat(ppts): add django template ppt
    Signed-off-by: misilelab <misileminecord@gmail.com>

---
+++ projects/dsb/ppts/django/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Presentation, Slide } from '@animotion/core'
+</script>
+
+<div class="text-blue-400 text-green-400 underline border-black border-r-8' bg-green-400 bg-purple-400 bg-red-400" style="display: none;"></div>
+
+<Presentation options={{history: true, transition: 'none', progress: false, controls: false}}>
+  <Slide class="h-full place-content-center place-items-center">
+    <h1 class="text-8xl">Realtime chat app</h1>
+  </Slide>
+</Presentation>
