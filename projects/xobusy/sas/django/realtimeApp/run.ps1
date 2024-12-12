cdf6bb0aabd2b7c83eb595de935d4d71eef18699
misilelab <misileminecord@gmail.com>
2024-12-12 14:13:19 +0900

    fix(django): venv setup
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/xobusy/sas/django/realtimeApp/run.ps1
+++ projects/xobusy/sas/django/realtimeApp/run.ps1
@@ -1,3 +1,4 @@
 python -m venv .venv
-.\.venv\bin\pip install -r requirements.txt
-.\.venv\bin\daphne -p 8000 realtimeApp.asgi:application
+.\.venv\Scripts\activate
+pip install -r requirements.txt
+daphne -p 8000 realtimeApp.asgi:application
