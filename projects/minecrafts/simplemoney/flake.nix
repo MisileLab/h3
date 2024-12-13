a77ea3f464198b60ce32522c6cf5139a8a20c10b
misilelab <misileminecord@gmail.com>
2024-12-13 14:11:01 +0900

    chore: upgrade python
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/minecrafts/simplemoney/flake.nix
+++ projects/minecrafts/simplemoney/flake.nix
@@ -25,7 +25,7 @@
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
           # Add any you need here
-          packages = with pkgs; [ python312Full ];
+          packages = with pkgs; [ gcc ];
 
           # Set any environment variables for your dev shell
           env = { };
