7da9beaaf95d5de61ac13bf6ce3897a362eef376
misilelab <misileminecord@gmail.com>
2024-12-15 03:44:02 +0900

    fix(meteorite): enable lunch api
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/meteorite/main.py
+++ projects/dsb/meteorite/main.py
@@ -3,4 +3,4 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
-# app.include_router(LunchRouter)
+app.include_router(LunchRouter)
