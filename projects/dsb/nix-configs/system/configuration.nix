402e04c92b7336c968d8325ed861a2c7bf4477d0
misilelab <misileminecord@gmail.com>
2024-12-15 15:53:37 +0900

    feat(nix): make many ignoring paths
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/configuration.nix
+++ projects/dsb/nix-configs/system/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, options, ... }:
 {
   imports = [
     ./common.nix
@@ -16,6 +16,48 @@
       package = pkgs.plocate;
       localuser = null;
       interval = "never";
+      prunePaths = options.services.locate.prunePaths.default ++ ([
+        "/home/misile/.cargo"
+        "/home/misile/.config/unity3d/cache"
+        "/home/misile/.local/share/pnpm"
+        "/home/misile/.docker"
+        "/home/misile/.dotnet"
+        "/home/misile/.gradle"
+        "/home/misile/.tor project"
+        "/home/misile/.mozilla"
+        "/home/misile/.net"
+        "/home/misile/.npm"
+        "/home/misile/.nuget"
+        "/home/misile/.radicle"
+        "/home/misile/.rustup"
+        "/home/misile/.vscode-oss"
+        "/home/misile/.vscode"
+        "/home/misile/.volta"
+        "/home/misile/.var"
+        "/home/misile/non-nixos-things/waydroid_script"
+        "/home/misile/go"
+        "/home/misile/micromamba"
+        "/home/misile/Unity"
+        "/home/misile/Documents"
+        "/var/lib/waydroid"
+        "/var/lib/flatpak"
+        "/var/lib/docker"
+        "/var/lib/clamav"
+        "/var/log"
+        "/root"
+      ]);
+      pruneNames = options.services.locate.pruneNames.default ++ ([
+        "node_modules"
+        ".wine"
+        ".venv"
+        ".mypy_cache"
+        "media"
+        "__pycache__"
+        ".ruff_cache"
+        ".svelte-kit"
+        ".zig-cache"
+        "zig-cache"
+      ]);
     };
     gnome.gnome-keyring.enable = true;
     pcscd.enable = true;
