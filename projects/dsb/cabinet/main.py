4ed311da09eda021dfb85d3859095ad00b8bd1da
misilelab <misileminecord@gmail.com>
2024-12-24 14:03:47 +0900

    feat(cabinet): bruh stop too many requests
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cabinet/main.py
+++ projects/dsb/cabinet/main.py
@@ -1,6 +1,7 @@
 from googlesearch import search # pyright: ignore[reportMissingTypeStubs, reportUnknownVariableType]
 
 from os import getenv
+from csv import DictWriter
 
 proxy_url = getenv("PROXY_URL")
 proxy_user = getenv("PROXY_USERNAME")
@@ -15,9 +16,13 @@ print(proxy)
 base_query = "site:x.com"
 suicidal = base_query + " 자살"
 
-data_num = 100
+data_num = 4000
 
-result = list(search(suicidal, advanced=True, unique=True, num_results=data_num, sleep_interval=5, lang="ko", safe=None, ssl_verify=None, proxy=proxy)) # pyright: ignore[reportArgumentType]
-for i in result:
-  print(i.title) # pyright: ignore[reportAny]
+result = list(search(suicidal, advanced=True, unique=True, num_results=data_num, sleep_interval=60, lang="ko", safe=None, ssl_verify=None, proxy=proxy)) # pyright: ignore[reportArgumentType]
+with open("data.csv", "w", newline='') as f:
+  dw = DictWriter(f, fieldnames=["title", "url", "description"])
+  dw.writeheader()
+  for i in result:
+    print(i.title) # pyright: ignore[reportAny]
+    dw.writerow({"title": i.title, "url": i.url, "description": i.description}) # pyright: ignore[reportAny]
 
