90e1ec3f5a5e78fe08a8cf15dfbc5ad426e62e93
misilelab <misileminecord@gmail.com>
2024-12-26 00:49:26 +0900

    fix(nix): revert only fonts
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/fonts.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/fonts.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    nerd-fonts.fira-code nanum pretendard noto-fonts noto-fonts-color-emoji
+    nerd-fonts.fira-code nanum pretendard noto-fonts-color-emoji
+    noto-fonts noto-fonts-cjk-sans
   ];
 }
