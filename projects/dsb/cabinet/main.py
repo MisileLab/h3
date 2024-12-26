13193c7b6a4f222ebb81f4c2679cbbff72212da2
misilelab <misileminecord@gmail.com>
2024-12-26 13:41:08 +0900

    feat(cabinet): proper logger and duct-tape
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cabinet/main.py
+++ projects/dsb/cabinet/main.py
@@ -1,6 +1,7 @@
 from googlesearch import search # pyright: ignore[reportMissingTypeStubs, reportUnknownVariableType]
 from requests.exceptions import HTTPError, ProxyError
 from requests import Timeout
+from loguru import logger
 
 from os import getenv
 from csv import DictWriter
@@ -16,20 +17,23 @@ if None in [proxy_url, proxy_user, proxy_pass]:
   proxy = None
 else:
   proxy = f"http://{proxy_user}:{proxy_pass}@{proxy_url}"
-print(proxy)
+logger.info(proxy)
 
 base_query = "site:x.com"
 suicidal = f"{base_query} #자살"
+logger.debug(suicidal)
 
 data_num = 4000
-result_interval = 10
+result_interval = 100
 sleep_interval_min = 0
-sleep_interval_max = 60
+sleep_interval_max = 20
 _start = getenv("start_num")
-start_num = 1 if _start is None else int(_start)
+start_num = 0 if _start is None else int(_start)
 
 def search_res(query: str, start_num: int):
-  return list(search(query, advanced=True, unique=True, num_results=result_interval, start_num=start_num, lang="ko", safe=None, ssl_verify=None, proxy=proxy)) # pyright: ignore[reportArgumentType]
+  res = list(search(query, advanced=True, region="kr", num_results=result_interval, start_num=start_num, safe=None, ssl_verify=None, proxy=proxy)) # pyright: ignore[reportArgumentType]
+  logger.debug(res)
+  return res
 
 with open("suicidal.csv", "w", newline='') as f:
   dw = DictWriter(f, fieldnames=["title", "url", "description"])
@@ -37,24 +41,27 @@ with open("suicidal.csv", "w", newline='') as f:
   i = start_num
   while i <= data_num:
     try:
-      print(i, "begin")
+      logger.debug(f"{i}, begin")
       datas = search_res(suicidal, i)
-      print(i, "end")
+      logger.debug(f"{i}, end")
     except HTTPError as e:
       if e.response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-        print("retry after 10 mins")
+        logger.warn("retry after 10 mins")
         sleep(60 * 10)
         continue
       raise e
     except (Timeout, ProxyError):
-      print("let's try another interval")
+      logger.warn("let's try another interval")
       sleep(10)
       continue
+    if datas == []:
+      print("data is None")
+      break
     for data in datas:
-      print(data.title) # pyright: ignore[reportAny]
+      logger.info(data.title) # pyright: ignore[reportAny]
       dw.writerow({"title": data.title, "url": data.url, "description": data.description}) # pyright: ignore[reportAny]
     i += result_interval
     r = SystemRandom().randint(sleep_interval_min, sleep_interval_max)
-    print(f"rest for {r}")
+    logger.debug(f"sleep {r} secs")
     sleep(r)
 
