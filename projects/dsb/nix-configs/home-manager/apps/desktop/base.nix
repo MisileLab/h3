35492fe2bd877c91f07244dba9194e5068b48fb9
misilelab <misileminecord@gmail.com>
2025-01-01 12:57:12 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -1,4 +1,4 @@
-{pkgs, stablep, ...}:
+{pkgs, ...}:
 {
   imports = [
     ./security.nix
@@ -11,7 +11,7 @@
     packages = with pkgs; [
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick virt-manager xfce.thunar
-      galaxy-buds-client kdePackages.filelight (stablep.firefoxpwa) gparted
+      galaxy-buds-client kdePackages.filelight firefoxpwa gparted
       gimp telegram-desktop xournalpp exodus
     ];
     pointerCursor = {
@@ -37,8 +37,8 @@
     };
     firefox = {
       enable = true;
-      package = stablep.firefox;
-      nativeMessagingHosts = [stablep.firefoxpwa];
+      package = pkgs.firefox;
+      nativeMessagingHosts = with pkgs; [firefoxpwa];
     };
   };
   xdg = {
