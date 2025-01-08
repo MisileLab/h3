a9481de1b3a8ff0eaa0f0b43e0f1f511bb78f14c
misilelab <misileminecord@gmail.com>
2025-01-09 01:40:08 +0900

    feat(cabinet): add flake and gitignore
    Signed-off-by: misilelab <misileminecord@gmail.com>

---
+++ projects/dsb/cabinet/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Environment for cabinet";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib";
+          '';
+        };
+      });
+    };
+}
