678b374b272ca21dcc2a9b04eedf04553f2d7752
misilelaboratory <misileminecord@gmail.com>
2025-01-10 02:13:41 +0900

    feat(dangling-point): my game design changed
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/dangling-point/Assets/Scripts/TileMapScript.cs
+++ projects/dsb/dangling-point/Assets/Scripts/TileMapScript.cs
@@ -1,21 +1,26 @@
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
-using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public enum TileType {
-  // Non machine
-  Empty,
-  Wire,
+public enum Material {
+  material
+}
 
-  // machine
-  Producer,
-  Launcher
+public enum ItemType {
+  material,
+  researcher,
+  reformer,
+  extractor
+}
+
+public class Item {
+  public ItemType type;
+  public int amount;
 }
 
 public class Tile {
-  public TileType type;
+  public Material material;
+  public int amount;
   public int x;
   public int y;
 }
@@ -24,27 +29,35 @@ public class TileMapScript: MonoBehaviour
 {
   private Tilemap tilemap;
   private Dictionary<Vector3Int, Tile> tiles = new();
+  private List<Item> items = new();
   private Vector3Int pastTilePos;
 
   // tiles
   public TileBase normalTile;
   public TileBase hoveredTile;
+  public TileBase researchedTile;
+  public TileBase emptyTile;
 
-  bool isSamePos(Vector3Int a, Vector3Int b) {
+  bool IsSamePos(Vector3Int a, Vector3Int b) {
     return a.x == b.x && a.y == b.y;
   }
 
-  void render() {
+  void Render() {
     foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin) {
-      Debug.Assert(tilemap.HasTile(pos));
-      tiles[pos] = new Tile { type = TileType.Empty, x = pos.x, y = pos.y };
+      if (tiles.ContainsKey(pos)) {
+        if (tiles[pos].amount == 0) {
+          tilemap.SetTile(pos, emptyTile);
+        } else {
+          tilemap.SetTile(pos, researchedTile);
+        }
+      }
     }
   }
 
   void Start() {
     tilemap = gameObject.GetComponent<Tilemap>();
     Debug.Assert(tilemap != null);
-    render();
+    Render();
   }
 
   // Update is called once per frame
@@ -55,7 +68,7 @@ public class TileMapScript: MonoBehaviour
     if (tile) {
       if (Input.GetMouseButtonDown(0)) {
         Debug.Log("clicked");
-      } else if (!isSamePos(tilemapPos, pastTilePos)) {
+      } else if (!IsSamePos(tilemapPos, pastTilePos)) {
         tilemap.SetTile(tilemapPos, hoveredTile);
         if (pastTilePos != null) {
           tilemap.SetTile(pastTilePos, normalTile);
