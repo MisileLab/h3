32490162ef1a23d1a918fb78820a8d6d87cd7aa4
misilelab <misileminecord@gmail.com>
2025-01-18 01:18:01 +0900

    feat(cabinet): deduplicate script for normal
    Signed-off-by: misilelab <misileminecord@gmail.com>

---
+++ projects/dsb/cabinet/deduplicate_normal.py
@@ -0,0 +1,16 @@
+from csv import DictReader, DictWriter
+from pathlib import Path
+
+writed: list[str] = []
+
+with open("normal.csv", "r", newline="") as f:
+  dr = DictReader(f)
+  with open("result_normal.csv", "w", newline="") as res:
+    dw = DictWriter(res, fieldnames=Path("suicidal.csv").read_text().split("\n")[0].split(","))
+    dw.writeheader()
+    for i in dr:
+      if i["url"] in writed:
+        print("skip")
+        continue
+      dw.writerow(i)
+      writed.append(i["url"])
