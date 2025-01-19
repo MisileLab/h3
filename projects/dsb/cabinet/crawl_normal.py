253048cc347d69975af0cdc47feb6792894171d0
misilelab <misileminecord@gmail.com>
2025-01-19 13:21:54 +0900

    fix(cabinet): maybe this loop can happen
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cabinet/crawl_normal.py
+++ projects/dsb/cabinet/crawl_normal.py
@@ -17,15 +17,18 @@ proxy = get_proxy()
 logger.info(proxy)
 api = API(proxy=proxy)
 
-class NotEnoughData(Exception):
-  pass
-
 sleep_interval_min = 0
 sleep_interval_max = 20
 min_depth = 1
 max_depth = 3
 max_following_count = 50
 max_user_follower_count = 5000
+ignore_list: set[int] = set()
+
+class NotEnoughData(Exception):
+  def __init__(self, userid: int):
+    super().__init__()
+    ignore_list.add(userid)
 
 async def search_res(userid: int, max_depth: int, depth: int = 0) -> User | None:
   sleep_sec = SystemRandom().randint(1, 3)
@@ -37,24 +40,28 @@ async def search_res(userid: int, max_depth: int, depth: int = 0) -> User | None
   user = await api.user_by_id(userid) # pyright: ignore[reportUnknownMemberType]
   if user is None:
     return None
-  followings: list[User] = []
+  followings_set: set[User] = set()
   async for i in api.following(userid, limit=max_following_count): # pyright: ignore[reportUnknownMemberType]
-    followings.append(i)
-  if len(followings) == 0:
+    followings_set.add(i)
+  followings_list: list[User] = list(followings_set)
+  for i in followings_list:
+    if i.id in ignore_list:
+      followings_list.remove(i)
+  if len(followings_list) == 0:
     logger.warning("user has no following")
-    raise NotEnoughData()
-  selected_following = SystemRandom().choice(followings)
+    raise NotEnoughData(userid)
+  selected_following = SystemRandom().choice(followings_list)
   while selected_following.verified or selected_following.followersCount > max_user_follower_count:
-    if len(followings) == 1:
+    if len(followings_list) == 1:
       logger.warning("user has only non-normal users")
-      raise NotEnoughData()
+      raise NotEnoughData(selected_following.id)
     logger.info(f"skipping {selected_following.displayname}")
     if selected_following.verified:
       logger.info(f"{selected_following.displayname} is verified")
     else:
       logger.info(f"{selected_following.displayname} has too many followers ({selected_following.followersCount})")
-    followings.remove(selected_following)
-    selected_following = SystemRandom().choice(followings)
+    followings_list.remove(selected_following)
+    selected_following = SystemRandom().choice(followings_list)
   logger.debug(f"selected: {selected_following.displayname}")
   try:
     res = await search_res(selected_following.id, max_depth, depth+1)
@@ -63,7 +70,7 @@ async def search_res(userid: int, max_depth: int, depth: int = 0) -> User | None
     if depth-1 > min_depth:
       return await search_res(userid, max_depth, depth-1)
     else:
-      raise NotEnoughData
+      raise NotEnoughData(selected_following.id)
   if res is None and depth > min_depth and depth < max_depth:
     logger.warning("res's return is None")
     try:
@@ -73,7 +80,7 @@ async def search_res(userid: int, max_depth: int, depth: int = 0) -> User | None
       if depth-1 > min_depth:
         return await search_res(userid, max_depth, depth-1)
       else:
-        raise NotEnoughData
+        raise NotEnoughData(userid)
   return res if res is not None else user
 
 exist = Path("normal.csv").is_file()
