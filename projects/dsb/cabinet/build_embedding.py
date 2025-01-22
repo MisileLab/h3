cd9b73502363e725cd5507fb056f3f44b21b9e42
misilelab <misileminecord@gmail.com>
2025-01-23 03:44:55 +0900

    feat(cabinet): seperate suicidal and non-suicidal embedding
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cabinet/build_embedding.py
+++ projects/dsb/cabinet/build_embedding.py
@@ -1,22 +1,29 @@
 from json import dumps
 from pathlib import Path
 from pickle import loads
+from dataclasses import dataclass
 
 from twscrape import Tweet # pyright: ignore[reportMissingTypeStubs]
 
-data: dict[int, list[Tweet]] = {}
+@dataclass
+class Data:
+  t: list[Tweet]
+  suicidal: bool
+
+data: dict[int, Data] = {}
 
 for i in Path("results").glob("*.pkl"):
-  data[int(i.name.strip(".pkl"))] = loads(i.read_bytes())
+  data[int(i.name.strip(".pkl"))] = Data(loads(i.read_bytes()), False) # pyright: ignore[reportAny]
 
 for i in Path("results_normal").glob("*.pkl"):
-  data[int(i.name.strip(".pkl"))] = loads(i.read_bytes())
+  data[int(i.name.strip(".pkl"))] = Data(loads(i.read_bytes()), False) # pyright: ignore[reportAny]
 
 Path("embeddings").mkdir(exist_ok=True)
 
 for k, v in data.items():
-  with open(Path("embeddings", f"{k}.jsonl"), "w") as f:
-    for n, i in enumerate(v):
+  # TODO: check token limit when build embedding and if exceeds, split jsonl
+  with open(Path("embeddings", f"{'suicidal' if v.suicidal else 'normal'}{k}.jsonl"), "w") as f:
+    for n, i in enumerate(v.t):
       _ = f.write(dumps({
           "custom_id": f"request-{n}",
           "method": "POST",
