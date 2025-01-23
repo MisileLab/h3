be6198aa4f8a30e4ef511fc21e18871d6328ec02
misilelab <misileminecord@gmail.com>
2025-01-23 19:42:31 +0900

    feat(nix): install nushell completions
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/base.nix
@@ -1,6 +1,12 @@
-{pkgs, zigpkgs, secrets, ...}:
+{lib, pkgs, zigpkgs, secrets, ...}:
 let
   writeScript = name: content: pkgs.writeShellScriptBin name "#!${pkgs.nushell}/bin/nu\n${content} $@";
+  completions = [
+    "eza" "dotnet" "docker" "gh" "git" "glow" "just" "less" "man"
+    "nano" "nix" "npm" "pnpm" "pre-commit" "rustup" "rg" "ssh"
+    "tar" "vscode" "zellij" "curl" "bat" "cargo"
+  ];
+  aliases = ["docker"];
 in
   {
     imports = [
@@ -10,20 +16,27 @@ in
       ./network.nix
       ./utils.nix
     ];
-    home.packages = with pkgs; [
-      sbctl bluez cryptsetup smartmontools borgbackup rclone pulsemixer
-      portablemc miniserve openssl transmission
-      yt-dlp magic-wormhole ansifilter b3sum git-crypt inxi
-      (writeScript "manual" ''
-        ${pkgs.glow}/bin/glow -p ~/.config/home-manager/manual.md
-      '')
-      (writeScript "nix-clean" "nix-collect-garbage -d && sudo nix-collect-garbage -d && sudo nix store optimise")
-      (writeScript "cat" "${bat}/bin/bat")
-      (writeScript "ocat" "${coreutils}/bin/cat")
-      (writeScript "lzg" "${lazygit}/bin/lazygit")
-      (writeScript "utils" "~/repos/h3/projects/dsb/utils/zig-out/bin/utils")
-      (writeScript "zig-beta" "${zigpkgs.master}/bin/zig")
-    ];
+    home = {
+      packages = with pkgs; [
+        sbctl bluez cryptsetup smartmontools borgbackup rclone pulsemixer
+        portablemc miniserve openssl transmission
+        yt-dlp magic-wormhole ansifilter b3sum git-crypt inxi
+        (writeScript "manual" ''
+          ${pkgs.glow}/bin/glow -p ~/.config/home-manager/manual.md
+        '')
+        (writeScript "nix-clean" "nix-collect-garbage -d && sudo nix-collect-garbage -d && sudo nix store optimise")
+        (writeScript "cat" "${bat}/bin/bat")
+        (writeScript "ocat" "${coreutils}/bin/cat")
+        (writeScript "lzg" "${lazygit}/bin/lazygit")
+        (writeScript "utils" "~/repos/h3/projects/dsb/utils/zig-out/bin/utils")
+        (writeScript "zig-beta" "${zigpkgs.master}/bin/zig")
+      ];
+      activation = {
+        installCompletions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+          ${pkgs.uv}/bin/uv generate-shell-completion nushell > /home/misile/non-nixos-things/scripts/uv-completions.nu
+        '';
+      };
+    };
     programs = {
       gpg = {
         enable = true;
@@ -45,6 +58,12 @@ in
           ssh = "${pkgs.kitty}/bin/kitten ssh";
         };
         extraConfig = ''
+# completion that command name and program different
+source ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/tealdeer/tldr-completions.nu
+${lib.concatStringsSep "\n" (map (name: "source ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/${name}/${name}-completions.nu") completions)}
+${lib.concatStringsSep "\n" (map (name: "source ${pkgs.nu_scripts}/share/nu_scripts/aliases/${name}/${name}-aliases.nu") aliases)}
+# custom completions
+source ~/non-nixos-things/scripts/uv-completions.nu
 use std/util "path add"
 $env.UV_PUBLISH_TOKEN = "${secrets.UV_PUBLISH_TOKEN or "you_need_to_change_in_secrets_nix"}"
 $env.config.hooks.command_not_found = {
