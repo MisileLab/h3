c2551855e1654200f3344050839c1f61ec5b6eab
misilelab <misileminecord@gmail.com>
2025-01-24 15:27:27 +0900

    chore: migrate tailwindcss to v4; preparing redesign
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/schale/frontend/astro.config.mjs
+++ projects/schale/frontend/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://misile.xyz",
-  integrations: [tailwind(), sitemap({
+  integrations: [sitemap({
     filter: (route) => {
       let r = route.replace('https://misile.xyz', '')
       const vs = ["/data/news"]
@@ -23,5 +23,8 @@ export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
