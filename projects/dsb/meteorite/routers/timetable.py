a3176439fa59fe7242316e15da5e85b60bd29ba0
misilelab <misileminecord@gmail.com>
2025-02-03 13:01:38 +0900

    fix(meteorite)!: change parameter's name to more const name
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/meteorite/routers/timetable.py
+++ projects/dsb/meteorite/routers/timetable.py
@@ -20,9 +20,9 @@ class ClassInfo(BaseClass):
 
 @app.get("/timetable", description="returns dict, key is range of 1(monday) to 5(friday), value is list of classes")
 async def get_timetable(
-  name: Annotated[str, Field(description="name of school, can be autocompleted")],
+  school: Annotated[str, Field(description="name of school, can be autocompleted")],
   grade: Annotated[int, Field(description="grade of timetable")],
   s_class: Annotated[int, Field(description="class of timetable", alias="class")],
   next_week: Annotated[bool, Field(description="if True, returns next week's timetable else, returns current week's timetable")] = False
 ) -> dict[int, list[TimeTableData]]:
-  return fetch_timetable(name, next_week=next_week).timetable[grade][s_class]
+  return fetch_timetable(school, next_week=next_week).timetable[grade][s_class]
