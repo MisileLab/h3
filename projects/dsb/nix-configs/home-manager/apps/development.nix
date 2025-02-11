41b15c199474829cc99c5a9bd0693bbd6dc1d588
misilelab <misileminecord@gmail.com>
2025-02-11 20:03:49 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/development.nix
+++ projects/dsb/nix-configs/home-manager/apps/development.nix
@@ -35,8 +35,7 @@
     ]
     ++ (with llvmPackages_latest; [libcxxClang openmp libunwind]) # llvm
     ++ (with nodePackages_latest; [nodejs typescript typescript-language-server svelte-language-server]) # nodejs
-    # https://github.com/NixOS/nixpkgs/pull/380668
-    ++ (with python313Packages; [pip virtualenv /*mitmproxy*/]); # python thing
+    ++ (with python313Packages; [pip virtualenv mitmproxy]); # python thing
     file = {
       ".config/process-compose/theme.yaml".source = config.lib.file.mkOutOfStoreSymlink "${builtins.fetchGit {
         url="https://github.com/catppuccin/process-compose";
