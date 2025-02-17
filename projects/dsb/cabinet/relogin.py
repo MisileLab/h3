ad5add6d25d7a3955f04479c2792e4799970e494
misilelaboratory <misileminecord@gmail.com>
2025-02-17 22:43:46 +0900

    fix(cabinet): make login better, idk
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- projects/dsb/cabinet/relogin.py
+++ projects/dsb/cabinet/relogin.py
@@ -1,8 +1,15 @@
+from loguru import logger
+
 from lib import get_usernames, api
 from asyncio import run
+from time import sleep
+from secrets import SystemRandom
 
 async def main():
-  await api.pool.relogin(await get_usernames())
+  for i in await get_usernames():
+    logger.info(f"Relogging {i}")
+    await api.pool.relogin([i])
+    sleep(SystemRandom().randint(1, 3))
 
 if __name__ == "__main__":
   run(main())
