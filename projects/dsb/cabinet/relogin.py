d063304f94c813ca2837865edde1b72c8aa49bcd
misilelaboratory <misileminecord@gmail.com>
2025-02-17 22:44:26 +0900

    revert "fix(cabinet): make login better, idk"
    This reverts commit ad5add6d25d7a3955f04479c2792e4799970e494.

--- projects/dsb/cabinet/relogin.py
+++ projects/dsb/cabinet/relogin.py
@@ -1,15 +1,8 @@
-from loguru import logger
-
 from lib import get_usernames, api
 from asyncio import run
-from time import sleep
-from secrets import SystemRandom
 
 async def main():
-  for i in await get_usernames():
-    logger.info(f"Relogging {i}")
-    await api.pool.relogin([i])
-    sleep(SystemRandom().randint(1, 3))
+  await api.pool.relogin(await get_usernames())
 
 if __name__ == "__main__":
   run(main())
