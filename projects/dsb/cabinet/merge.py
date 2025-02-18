c842550c2db9cf91f5e8f9e9948444c6c0711408
misilelab <misileminecord@gmail.com>
2025-02-18 17:27:22 +0900

    fix(cabinet): confirmed exists
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/cabinet/merge.py
+++ projects/dsb/cabinet/merge.py
@@ -5,6 +5,7 @@ from lib import read_pickle, write_to_pickle
 df = read_pickle("data.pkl")
 df_user: DataFrame = df.loc[df['confirmed']] # pyright: ignore[reportUnknownVariableType]
 del df_user['data']
+del df_user['confirmed']
 
 write_to_pickle(df_user, "user.pkl") # pyright: ignore[reportUnknownArgumentType]
 
