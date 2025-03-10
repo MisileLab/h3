91edebc5bf716d2cbc12313c219981d56ca5e4ed
misilelab <misileminecord@gmail.com>
2025-03-10 10:39:33 +0900

    fix(nix): nh home missing argument
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/build-configuration.sh
+++ projects/dsb/nix-configs/build-configuration.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd ./home-manager && rm result && nh home build -o result && attic push cache result && cd ..
-cd ./system && rm result && nh os build -o result && attic push cache result && cd ..
+cd ./home-manager && rm result && nh home build . -o result && attic push cache result && cd ..
+cd ./system && rm result && nh os build . -o result && attic push cache result && cd ..
 
