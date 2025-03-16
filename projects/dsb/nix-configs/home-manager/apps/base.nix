aa34bcc75fa6078e432df8affee1c42f13781f56
misilelab <misileminecord@gmail.com>
2025-03-16 16:58:48 +0900

    feat(nix): add distrobox
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/base.nix
@@ -21,6 +21,7 @@ in
         sbctl bluez cryptsetup smartmontools borgbackup rclone pulsemixer
         portablemc miniserve openssl transmission attic-client
         yt-dlp magic-wormhole ansifilter b3sum git-crypt inxi age sops
+        distrobox
         (writeScript "manual" ''
           ${pkgs.glow}/bin/glow -p ~/.config/home-manager/manual.md
         '')
