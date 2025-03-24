facea3e15ec01c9eabea7446097a560d917636e3
misilelab <misileminecord@gmail.com>
2025-03-24 20:07:19 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/flake.nix
+++ projects/dsb/nix-configs/system/flake.nix
@@ -3,22 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    stablep.url = "github:nixos/nixpkgs/nixos-unstable";
+    # stablep.url = "github:nixos/nixpkgs/nixos-unstable";
     # lanzaboote = {
     #   url = "github:nix-community/lanzaboote/v0.4.1";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, stablep, /*lanzaboote*/... }@_: {
+  outputs = { self, nixpkgs, /*stablep, *//*lanzaboote*/... }@_: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        {_module.args = {stablep = import stablep { system = "x86_64-linux"; };};}
-        # https://github.com/NixOS/nixpkgs/issues/392278
-        ({ stablep, ... }: {
-          nixpkgs.overlays = [ (final: prev: {auto-cpufreq = stablep.auto-cpufreq;}) ];
+        # {_module.args = {stablep = import stablep { system = "x86_64-linux"; };};}
+        ({ ... }: {
+          nixpkgs.overlays = [(final: prev: {
+            # FIX: https://github.com/NixOS/nixpkgs/issues/392278
+            auto-cpufreq = prev.auto-cpufreq.overrideAttrs (oldAttrs: {
+              postPatch =
+                oldAttrs.postPatch
+                + ''
+
+                  substituteInPlace pyproject.toml \
+                  --replace-fail 'psutil = "^6.0.0"' 'psutil = ">=6.0.0,<8.0.0"'
+                '';
+            });
+          })];
         })
         # lanzaboote.nixosModules.lanzaboote
         ({ pkgs, lib, ... }: {
