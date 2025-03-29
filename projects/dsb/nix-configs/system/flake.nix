abda725d9b22e26f811f3f9a896051a2e8afda3f
misilelab <misileminecord@gmail.com>
2025-03-29 13:17:03 +0900

    chore(nix): pin more heavy dependency
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/flake.nix
+++ projects/dsb/nix-configs/system/flake.nix
@@ -3,32 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    # stablep.url = "github:nixos/nixpkgs/nixos-unstable";
+    stablep.url = "github:nixos/nixpkgs/nixos-unstable";
     # lanzaboote = {
     #   url = "github:nix-community/lanzaboote/v0.4.1";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, /*stablep, *//*lanzaboote*/... }@_: {
+  outputs = { self, nixpkgs, stablep, /*lanzaboote*/... }@_: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        # {_module.args = {stablep = import stablep { system = "x86_64-linux"; };};}
-        # ({ ... }: {
-        #   nixpkgs.overlays = [(final: prev: {
-        #     auto-cpufreq = prev.auto-cpufreq.overrideAttrs (oldAttrs: {
-        #       postPatch =
-        #         oldAttrs.postPatch
-        #         + ''
-        #
-        #           substituteInPlace pyproject.toml \
-        #           --replace-fail 'psutil = "^6.0.0"' 'psutil = ">=6.0.0,<8.0.0"'
-        #         '';
-        #     });
-        #   })];
-        # })
+        {_module.args = {stablep = import stablep { system = "x86_64-linux"; };};}
+        ({ stablep, ... }: {
+          nixpkgs.overlays = [(final: prev: {
+            webkitgtk_4_1 = stablep.webkitgtk_4_1;
+          })];
+        })
         # lanzaboote.nixosModules.lanzaboote
         ({ pkgs, lib, ... }: {
           environment.systemPackages = [pkgs.sbctl];
