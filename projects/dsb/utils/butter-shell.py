ef3a0e9cc0f17f9c8feb18e27f1a4cb3f9431a45
misilelab <misileminecord@gmail.com>
2025-04-09 14:15:06 +0900

    feat(utils)!: cleanup
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/utils/butter-shell.py
+++
@@ -1,6 +0,0 @@
-from gradio_client import Client
-from os import environ
-
-a = input("enter message: ")
-print(Client("https://butter.misile.xyz", auth=(environ["INF_USER"], environ["INF_PASS"])).predict(content=a, user="misile", audio_path=None))
-
