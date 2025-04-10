1a334636739f02f9609d9ffae7b40f5c522f0a82
misilelab <misileminecord@gmail.com>
2025-04-11 08:17:32 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/veritas/build.sh
+++ projects/veritas/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-nom build -L .#nixosConfigurations.veritas.config.system.build.toplevel --show-trace
+nom build -L .#nixosConfigurations.veritas.config.system.build.toplevel --show-trace --option binary-caches https://cache.nixos.org
 
