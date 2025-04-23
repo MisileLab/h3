86e52c5ce5cd831bd8f3c37bca1695a35ce7e279
misilelab <misileminecord@gmail.com>
2025-04-23 23:39:13 +0900

    chore(nix): unpin dependency
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/compatibility.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/compatibility.nix
@@ -1,5 +1,5 @@
-{pkgs, stablep, ...}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    wineWowPackages.stable appimage-run bottles (stablep.libreoffice)
+    wineWowPackages.stable appimage-run bottles libreoffice
   ];
 }
