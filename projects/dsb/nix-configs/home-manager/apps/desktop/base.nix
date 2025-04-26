81e7016495da09fff85a0b8d9c6570ad337fc1de
misilelab <misileminecord@gmail.com>
2025-04-26 14:59:34 +0900

    chore(nix): pin heavy dependency
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, stablep, ...}:
 {
   imports = [
     ./security.nix
@@ -13,7 +13,7 @@
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick virt-manager xfce.thunar
       galaxy-buds-client firefoxpwa gparted
-      gimp telegram-desktop xournalpp zotero headsetcontrol
+      gimp (stablep.telegram-desktop) xournalpp zotero headsetcontrol
     ] ++ (with kdePackages; [filelight okular merkuro]);
     pointerCursor = {
       name = "Adwaita";
@@ -24,6 +24,7 @@
   programs = {
     obs-studio = {
       enable = true;
+      package = stablep.obs-studio;
     };
     ghostty = {
       enable = true;
