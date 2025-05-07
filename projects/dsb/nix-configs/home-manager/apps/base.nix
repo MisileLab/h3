19fd3f08b72fcb11e607cfb59042a67f47264ef9
misilelab <misileminecord@gmail.com>
2025-05-07 14:51:32 +0900

    feat(nix): pin EDITOR to nvim
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/base.nix
@@ -72,6 +72,7 @@ ${lib.concatStringsSep "\n" (map (name: "source ${pkgs.nu_scripts}/share/nu_scri
 use std/util "path add"
 $env.UV_PUBLISH_TOKEN = (cat ${config.sops.secrets.uv_pypi_token.path})
 $env.DEVSHELL_NO_MOTD = 1;
+$env.EDITOR = "nvim";
 def bulk-run [paths: list<string>, command: string, ...args] {
   mut output = {}
   for $dir in $paths {
