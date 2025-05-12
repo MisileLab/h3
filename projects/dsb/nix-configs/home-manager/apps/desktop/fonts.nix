8359e11c74abf89d824a64330c00e56ae4ce7cea
misilelab <misileminecord@gmail.com>
2025-05-12 13:07:01 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/fonts.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/fonts.nix
@@ -1,7 +1,7 @@
-{pkgs, stablep, ...}: {
+{pkgs, ...}: {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    nerd-fonts.fira-code nanum pretendard (stablep.noto-fonts-color-emoji)
+    nerd-fonts.fira-code nanum pretendard noto-fonts-color-emoji
     noto-fonts noto-fonts-cjk-sans
   ];
 }
