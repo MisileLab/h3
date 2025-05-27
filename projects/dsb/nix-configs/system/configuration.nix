3bb36ab73ea792b1054745de8144af6986816f45
misilelab <misileminecord@gmail.com>
2025-05-27 14:04:47 +0900

    feat(nix)!: remove virtualbox and libvirtd
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/configuration.nix
+++ projects/dsb/nix-configs/system/configuration.nix
@@ -83,22 +83,22 @@
 
   virtualisation = {
     docker.enable = true;
-    libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [(pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd];
-        };
-      };
-    };
-    virtualbox.host.enable = true;
+    # libvirtd = {
+    #   enable = true;
+    #   qemu = {
+    #     package = pkgs.qemu_kvm;
+    #     runAsRoot = true;
+    #     swtpm.enable = true;
+    #     ovmf = {
+    #       enable = true;
+    #       packages = [(pkgs.OVMF.override {
+    #         secureBoot = true;
+    #         tpmSupport = true;
+    #       }).fd];
+    #     };
+    #   };
+    # };
+    # virtualbox.host.enable = true;
   };
 
   nixpkgs.config.allowUnfree = true;
