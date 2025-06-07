3650e61837027dcffc2460c80eb474eadd62ebb8
misilelab <misileminecord@gmail.com>
2025-06-08 02:51:22 +0900

    feat(vivian): ad-hoc crawler
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/vivian/get_comments.py
+++ projects/dsb/vivian/get_comments.py
@@ -1,17 +1,91 @@
-from pyyoutube import Api # pyright: ignore[reportMissingTypeStubs]
-from polars import DataFrame, read_avro, concat
+from pydantic import BaseModel
+from pyyoutube import Api, Comment, CommentThread # pyright: ignore[reportMissingTypeStubs]
+from polars import DataFrame, read_avro, concat, col
 from os import getenv
+from pathlib import Path
 
 client = Api(api_key=getenv("YOUTUBE_API_KEY"))
 videos = read_avro("videos.avro")
-df = DataFrame()
+df = read_avro("comments.avro") if Path("comments.avro").exists() else DataFrame()
 
-def append(df: DataFrame, data: dict[str, object]) -> DataFrame:
-  return concat([df, DataFrame(data)], how="vertical", rechunk=True)
+class Author(BaseModel):
+  name: str
+  image_url: str
 
-# TODO: skip existing datas
+class Data(BaseModel):
+  comment_id: str
+  content: str
+  author: Author
+  video_id: str
+  parent_id: str | None = None
+
+def append(df: DataFrame, data: Data) -> DataFrame:
+  return concat([df, DataFrame(data.model_dump())], how="vertical", rechunk=True)
+
+def append_comment(df: DataFrame, comment: Comment, video_id: str) -> DataFrame:
+  snippet = comment.snippet
+  if snippet is None:
+    raise ValueError("Comment has no snippet")
+  comment_id = comment.id
+  if comment_id is None:
+    raise ValueError("Comment has no ID")
+  content = snippet.textDisplay
+  if content is None:
+    raise ValueError("Comment has no content")
+  authorDisplayName = snippet.authorDisplayName
+  if authorDisplayName is None:
+    raise ValueError("Comment has no author display name")
+  authorImageUrl = snippet.authorProfileImageUrl
+  if authorImageUrl is None:
+    raise ValueError("Comment has no author image URL")
+  return append(df, Data(
+    comment_id=comment_id,
+    content=content,
+    author=Author(
+      name=authorDisplayName,
+      image_url=authorImageUrl
+    ),
+    parent_id=snippet.parentId,
+    video_id=video_id
+  ))
+
+def append_commentThreads(df: DataFrame, commentThread: CommentThread, video_id: str) -> DataFrame:
+  snippet = commentThread.snippet
+  if snippet is None:
+    raise ValueError("CommentThread has no snippet")
+  topLevelComment = snippet.topLevelComment
+  if topLevelComment is None:
+    raise ValueError("CommentThread has no top-level comment")
+  topLevelComment_id = topLevelComment.id
+  if topLevelComment_id is None:
+    raise ValueError("Top-level comment has no ID")
+  df = append_comment(df, topLevelComment, video_id)
+  _replies = commentThread.replies
+  replies = _replies.comments if _replies is not None else []
+  if not replies:
+    replies = []
+  if len(replies) == snippet.totalReplyCount:
+    for reply in replies:
+      df = append_comment(df, reply, video_id)
+  else:
+    comments = client.get_comments( # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
+      parts="id,snippet",
+      parent_id=topLevelComment_id,
+    )
+    if isinstance(comments, dict):
+      raise ValueError("Failed to fetch comments for parent ID: " + topLevelComment_id)
+    items = comments.items
+    if items is None:
+      items = []
+    for reply in items:
+      df = append_comment(df, reply, video_id)
+  return df
+
 for i in videos.iter_rows(named=True):
-  video_id: str = i["videoId"] # pyright: ignore[reportAny]
+  video_id: str = i["video_id"] # pyright: ignore[reportAny]
+  if df.filter(col("video_id") == video_id).height > 0: # pyright: ignore[reportUnknownMemberType]
+    continue
+  print(video_id)
   comments = client.get_comment_threads( # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
     video_id=video_id,
     count=None,
@@ -23,46 +97,7 @@ for i in videos.iter_rows(named=True):
 
   items = comments.items
   if not items:
-    print("No videos found.")
-    exit(1)
+    continue
 
   for i in items:
-    snippet = i.snippet
-    replies = i.replies
-    if replies is None or snippet is None:
-      continue
-    topLevelComment = snippet.topLevelComment
-    if topLevelComment is None:
-      continue
-    topLevelComment_id = topLevelComment.id
-    textDisplay = ""
-    if topLevelComment_id:
-      print("found top level comment")
-      topLevelComment = client.get_comment_by_id( # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
-        parts="snippet",
-        comment_id=topLevelComment_id
-      )
-      if not isinstance(topLevelComment, dict):
-        items = topLevelComment.items
-        if not items:
-          items = []
-        topLevelComment_snippet = items[0].snippet
-        if topLevelComment_snippet is None:
-          textDisplay = ""
-        else:
-          textDisplay = topLevelComment_snippet.textDisplay
-          if textDisplay is None:
-            textDisplay = ""
-    comments = replies.comments if replies.comments else []
-    df = append(df, {
-      "videoId": video_id,
-      "totalReplyCount": snippet.totalReplyCount,
-      "content": textDisplay,
-      "replies": replies.comments
-    })
-    df.write_avro("comments.avro")
-    if topLevelComment_id == snippet.totalReplyCount != len(comments):
-      print("get all comments of video")
-      comments = []
-      # TODO: get all comments
-
+    df = append_commentThreads(df, i, video_id)
