71c932733b9901959c32d335a474cf9efd21a11c
misilelab <misileminecord@gmail.com>
2025-06-07 19:02:21 +0900

    chore(crystal): shell.nix -> flake.nix
    Signed-off-by: misilelab <misileminecord@gmail.com>

---
+++ projects/dsb/crystal/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  # Flake inputs
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # The systems supported for this flake
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      });
+    };
+}
