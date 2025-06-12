ba466858e933fef7d346c33f652704ac0aaf1cb1
misilelab <misileminecord@gmail.com>
2025-06-13 03:45:34 +0900

    fix(vivian): jsonl parsing error
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/vivian/batch.py
+++ projects/dsb/vivian/batch.py
@@ -33,20 +33,24 @@ for i in (progress_bar := tqdm(list(Path("./batches").glob("*.jsonl")))):
     batch = o.batches.retrieve(batch_id)
   output_file_id = batch.output_file_id
   if output_file_id is not None:
-    output = loads(o.files.content(output_file_id).text) # pyright: ignore[reportAny]
-    comment_id: str = output["custom_id"] # pyright: ignore[reportAny]
-    response: str = output['response']['body']['choices'][0]['message']['content'].strip() # pyright: ignore[reportAny]
-    if response in ["A", "B"]:
-      is_bot = response == "A"
-      data = Data.model_validate(
-        comments.filter(col("comment_id") == comment_id).to_dicts()[0]
-      ).model_dump()
-      df = append(df, ProcessedData(
-        is_bot_comment=is_bot, **data # pyright: ignore[reportAny]
-      ))
-    _ = o.files.delete(output_file_id)
-    _ = o.files.delete(batch.input_file_id)
-    df.write_avro("processed.avro")
+    outputs = [
+      loads(i)
+      for i in o.files.content(output_file_id).text.split("\n")
+    ]
+    for output in outputs: # pyright: ignore[reportAny]
+      comment_id: str = output["custom_id"] # pyright: ignore[reportAny]
+      response: str = output['response']['body']['choices'][0]['message']['content'].strip() # pyright: ignore[reportAny]
+      if response in ["A", "B"]:
+        is_bot = response == "A"
+        data = Data.model_validate(
+          comments.filter(col("comment_id") == comment_id).to_dicts()[0]
+        ).model_dump()
+        df = append(df, ProcessedData(
+          is_bot_comment=is_bot, **data # pyright: ignore[reportAny]
+        ))
+      _ = o.files.delete(output_file_id)
+      _ = o.files.delete(batch.input_file_id)
+      df.write_avro("processed.avro")
   else:
     print("something wrong")
     exit(1)
