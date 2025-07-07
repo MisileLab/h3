e0967ce7051e1ddec0f5eea4808a77b370d7ec2c
misilelab <misileminecord@gmail.com>
2025-07-07 12:18:22 +0900

    feat(vivian)!: migrate to https://github.com/misilelab/noMoreSpam
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/vivian/main.py
+++
@@ -1,7 +0,0 @@
-from asyncio import run
-from importlib import import_module
-from sys import argv
-
-if __name__ == '__main__':
-  print("this is boilerplate because can't import correctly")
-  run(import_module(argv[1].replace("/", ".").strip(".py")).main()) # pyright: ignore[reportAny]
