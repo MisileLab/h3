429477bacacc685ec70061bc02e67af3f867df69
misilelab <misileminecord@gmail.com>
2025-07-09 14:38:33 +0900

    feat(nix): add act
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/development.nix
+++ projects/dsb/nix-configs/home-manager/apps/development.nix
@@ -10,7 +10,7 @@
       # Development
       gel d2 pre-commit just mongosh tabnine
       snyk radicle-node infisical pnpm poop binsider
-      typst
+      typst act
 
       # Language tools
       vscode-langservers-extracted
