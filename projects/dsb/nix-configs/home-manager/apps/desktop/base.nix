f6287c136538f551f2f987f49e137b2ab6c8d8c3
misilelab <misileminecord@gmail.com>
2025-07-15 12:30:02 +0900

    feat(nix): gimp2 -> gimp3
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/desktop/base.nix
@@ -13,7 +13,7 @@
       brightnessctl clipman wl-clipboard pavucontrol
       imagemagick loupe xfce.thunar
       galaxy-buds-client firefoxpwa gparted
-      gimp telegram-desktop xournalpp zotero headsetcontrol
+      gimp3 telegram-desktop xournalpp zotero headsetcontrol
     ] ++ (with kdePackages; [filelight okular merkuro]);
     pointerCursor = {
       name = "Adwaita";
