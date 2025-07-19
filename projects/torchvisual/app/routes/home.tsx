42d66b93bbc4abb26f0f72207ca3d13430e1036b
misilelab <misileminecord@gmail.com>
2025-07-20 02:36:01 +0900

    feat(torchvisual): proof-of-concept
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/torchvisual/app/routes/home.tsx
+++ projects/torchvisual/app/routes/home.tsx
@@ -2,6 +2,41 @@ import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from "@xyflow/
 import { useState, useCallback } from "react";
 import type { Route } from "./+types/home";
 import "@xyflow/react/dist/style.css";
+import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "~/components/ui/select";
+
+interface Argument {
+  name: string;
+  type: string;
+  description?: string;
+}
+
+interface Layer {
+  name: string;
+  arguments: Record<string, Argument>;
+}
+
+const Layers: Record<string, Layer> = {
+  linear: {
+    name: "Linear",
+    arguments: {
+      in_features: {
+        name: "in_features",
+        type: "number",
+        description: "Size of each input sample.",
+      },
+      out_features: {
+        name: "out_features",
+        type: "number",
+        description: "Size of each output sample.",
+      },
+      bias: {
+        name: "bias",
+        type: "boolean",
+        description: "If set to false, the layer will not learn an additive bias.",
+      },
+    },
+  }
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -19,6 +54,7 @@ export default function Home() {
 
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
+  const [selectedLayerType, setSelectedLayerType] = useState("linear");
  
   const onNodesChange = useCallback(
     // @ts-ignore - I gave up type annotation
@@ -37,15 +73,28 @@ export default function Home() {
   );
  
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      />
+    <div className="h-screen w-screen flex flex-row bg-background">
+      <div className="flex-1">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        />
+      </div>
+      <div className="bg-overlay1 w-3/12 h-full flex flex-col gap-4 px-3 py-3">
+        <h1 className="font-bold text-xl">Layer Type</h1>
+        <Select onValueChange={(value) => setSelectedLayerType(value)} value={selectedLayerType}>
+          <SelectTrigger className="w-full bg-white">
+            <SelectValue placeholder="Linear" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="linear">Linear</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 }
