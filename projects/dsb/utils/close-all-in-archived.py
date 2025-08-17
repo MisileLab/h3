2b0e26af3bfee6f0baa5b80ee997a05c1edff582
misilelab <misileminecord@gmail.com>
2025-08-18 00:55:26 +0900

    refactor(utils): close-all-in-archived optimize
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/utils/close-all-in-archived.py
+++ projects/dsb/utils/close-all-in-archived.py
@@ -10,15 +10,15 @@ for i in g.get_user().get_repos():
   if not i.archived:
     continue
   issues = i.get_issues(state="open")
-  if issues.totalCount == 0:
-    continue
   pr = i.get_pulls(state="open")
-  if i.archived and (issues.totalCount >= 0 or pr.totalCount >= 0):
-    i.edit(archived=False)
-    for j in issues:
-      print(f"closing issue {j.id}")
-      j.edit(state="closed")
-    for j in pr:
-      print(f"closing pr {j.id}")
-      j.edit(state="closed")
-    i.edit(archived=True)
+  print(issues.totalCount, pr.totalCount)
+  if issues.totalCount == 0 and pr.totalCount == 0:
+    continue
+  i.edit(archived=False)
+  for j in issues:
+    print(f"closing issue {j.id}")
+    j.edit(state="closed")
+  for j in pr:
+    print(f"closing pr {j.id}")
+    j.edit(state="closed")
+  i.edit(archived=True)
