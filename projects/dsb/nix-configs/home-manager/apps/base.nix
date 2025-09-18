f7b7f784218810de61400f2ff12bef356aca4974
misilelab <misileminecord@gmail.com>
2025-09-18 20:27:58 +0900

    feat(nix): add zoom-us
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/apps/base.nix
+++ projects/dsb/nix-configs/home-manager/apps/base.nix
@@ -23,7 +23,7 @@ in
         sbctl bluez cryptsetup smartmontools borgbackup rclone pulsemixer
         portablemc miniserve openssl transmission attic-client
         yt-dlp magic-wormhole ansifilter b3sum git-crypt inxi age sops
-        distrobox vulnix zap
+        distrobox vulnix zap zoom-us
         (writeScript "manual" ''
           ${pkgs.glow}/bin/glow -p ~/.config/home-manager/manual.md
         '')
