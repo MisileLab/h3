dd3c6333521cb4c7f58ffb3ca892874b82109a43
misilelab <misileminecord@gmail.com>
2025-10-02 08:28:25 +0900

    chore(nix): pin heavy dependency
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/flake.nix
+++ projects/dsb/nix-configs/home-manager/flake.nix
@@ -7,7 +7,7 @@
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # stable.url = "github:nixos/nixpkgs/nixos-unstable";
     # nur.url = "github:nix-community/NUR";
     catppuccin = {
       url = "github:catppuccin/nix";
@@ -30,15 +30,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, stable, home-manager, catppuccin, zig, nix-index-database, sops-nix, niri, ... }:
+  outputs = { nixpkgs, /*stable, */home-manager, catppuccin, zig, nix-index-database, sops-nix, niri, ... }:
     let
       system = "x86_64-linux"; # replace with your system
       overlays = [
         (final: prev: {
-          firefox = stablep.firefox;
-          firefox-unwrapped = stablep.firefox-unwrapped;
-          # https://github.com/NixOS/nixpkgs/issues/447625
-          fish = stablep.fish;
           dart = prev.dart.overrideAttrs (old: {
             installPhase = ''
               runHook preInstall
@@ -59,7 +55,7 @@
       };
       zigpkgs = zig.packages."${system}";
       c = import ./config.nix;
-      stablep = import stable {inherit system;config = {allowUnfree = true;};};
+      # stablep = import stable {inherit system;config = {allowUnfree = true;};};
     in {
       nixpkgs.overlays = overlays;
       homeConfigurations."misile" = home-manager.lib.homeManagerConfiguration {
