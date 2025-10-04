1a15f4c7cc87caa18ec80e2db3df1b3bcd2d97ab
misilelab <misileminecord@gmail.com>
2025-10-04 20:28:24 +0900

    fix(nix): deprecated module
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/system/configuration.nix
+++ projects/dsb/nix-configs/system/configuration.nix
@@ -89,13 +89,6 @@
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [(pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd];
-        };
       };
     };
     spiceUSBRedirection.enable = true;
