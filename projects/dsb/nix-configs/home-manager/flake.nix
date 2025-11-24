86dc62efa14bb6d1cd6762818cb7d66c2b1639aa
misilelab <misileminecord@gmail.com>
2025-11-25 08:58:07 +0900

    chore(nix): update flake
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- projects/dsb/nix-configs/home-manager/flake.nix
+++ projects/dsb/nix-configs/home-manager/flake.nix
@@ -7,7 +7,7 @@
       url = "github:mitchellh/zig-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # stable.url = "github:nixos/nixpkgs/nixos-unstable";
     # nur.url = "github:nix-community/NUR";
     nix-ai-tools = {
       url = "github:numtide/nix-ai-tools";
@@ -34,14 +34,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, stable, home-manager, catppuccin, zig, nix-index-database, sops-nix, niri, nix-ai-tools, ... }:
+  outputs = { nixpkgs, /*stable, */home-manager, catppuccin, zig, nix-index-database, sops-nix, niri, nix-ai-tools, ... }:
     let
       system = "x86_64-linux"; # replace with your system
       overlays = [
         (final: prev: {
           codex = nix-ai-toolspkgs.codex;
           claude-code = nix-ai-toolspkgs.claude-code;
-          # something is not stable
           opencode = nix-ai-toolspkgs.opencode;
           dart = prev.dart.overrideAttrs (old: {
             installPhase = ''
@@ -64,7 +63,7 @@
       nix-ai-toolspkgs = nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system};
       zigpkgs = zig.packages."${system}";
       c = import ./config.nix;
-      stablep = import stable {inherit system;config = {allowUnfree = true;};};
+      # stablep = import stable {inherit system;config = {allowUnfree = true;};};
     in {
       nixpkgs.overlays = overlays;
       homeConfigurations."misile" = home-manager.lib.homeManagerConfiguration {
@@ -79,7 +78,7 @@
         extraSpecialArgs = {
           inherit c;
           inherit zigpkgs;
-          inherit stablep;
+          # inherit stablep;
         };
       };
     };
